2be87db251b6ad3297090fb7d73aa3dc366ef19f
Rebecca <rebecca90469@gmail.com>
2024-07-24 13:20:04 -0400

    Set up for some routes and more basic file structure

--- server/src/server_index.js
+++ server/src/server_index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { config } from 'dotenv';
+import chatsRoutes from './routes/chats_routes.js';
+import projectsRoutes from './routes/projects_routes.js';
 import db from './db/connection.js';
 
 config();
@@ -10,6 +12,9 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Welcome to CoLab!');
 });
+
+app.use("/chats", chatsRoutes)
+app.use("/projects", projectsRoutes)
 
 app.listen(PORT, () => {
   console.log('Server is running on port', PORT);
