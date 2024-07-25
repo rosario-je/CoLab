5601ba2232374e08a776e3ca69e614677880625b
Rebecca <rebecca90469@gmail.com>
2024-07-25 12:59:04 -0400

    Setup for routes to implement later

--- server/src/db/queries/project_queries.js
+++ server/src/db/queries/project_queries.js
@@ -41,4 +41,54 @@ const getAllProjects = async () => {
 //   }
 // };
 
+// This will be for creating a new project
+
+// const createNewProject = async (name, description, user_id, max_participants, github_repo) => {
+//   try {
+//     const data = await db.query(
+//       `INSERT INTO projects (name, description, owner_id, max_participants, github_repo)
+//       VALUES ($1, $2, $3, $4, $5)
+//       RETURNING *`,
+//       [name, description, user_id, max_participants, github_repo]
+//     );
+//     return data.rows[0];
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// This will be for updating a project when the owner doesn't want to add more participants; only the owner can do this
+// const noNewParticipants = async (project_id, user_id) => {
+//   try {
+//     const data = await db.query(
+//       `UPDATE projects
+//       SET is_accepting_users = false
+//       WHERE project_id = $1
+//       AND owner_id = $2
+//       RETURNING *`,
+//       [project_id]
+//     );
+//     return data.rows[0];
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// This will be for updating a project when it is completed; only the owner can do this
+// const projectCompleted = async (project_id, user_id) => {
+//   try {
+//     const data = await db.query(
+//       `UPDATE projects
+//       SET is_completed = true
+//       WHERE project_id = $1
+//       AND owner_id = $2
+//       RETURNING *`,
+//       [project_id]
+//     );
+//     return data.rows[0];
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
 export { getAllProjects };
--- server/src/routes/projects_routes.js
+++ server/src/routes/projects_routes.js
@@ -1,4 +1,13 @@
 import express from 'express';
 const router = express.Router();
 
+// http://localhost:5000/projects/create
+router.get ('/create', (req, res) => {
+  res.send('Create a new project');
+});
+
+// http://localhost:5000/projects/create/:id
+router.post ('/create/:id', (req, res) => {
+  res.send('Project created');
+});
 export default router;
