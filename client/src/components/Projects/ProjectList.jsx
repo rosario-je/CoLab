e1605ffcd95dba010d760ab8dc8a56fee17be021
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-25 01:15:14 -0400

    refactor: Update ProjectList component layout and responsiveness

--- client/src/components/Projects/ProjectList.jsx
+++ client/src/components/Projects/ProjectList.jsx
@@ -3,7 +3,8 @@ import { ProjectCard } from './ProjectCard'
 
 export const ProjectList = () => {
   return (
-    <div className='w-[90%]'>
+    <div className='w-11/12'>
+      <ProjectCard />
       <ProjectCard />
     </div>
   )
