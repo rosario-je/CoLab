f75fedd37a5a5e671df8c11956d20d04c9021057
Rebecca <rebecca90469@gmail.com>
2024-07-25 00:27:40 -0400

    Seed data missed in last push

--- server/src/db/seeds/TEMP_USER_SEEDS.sql
+++ server/src/db/seeds/TEMP_USER_SEEDS.sql
@@ -1,7 +1,10 @@
 INSERT INTO temp_users (name, username, email) VALUES
-('Alice Johnson', 'alicej', 'alice.johnson@example.com'),
-('Bob Smith', 'bobsmith', 'bob.smith@example.com'),
-('Carol White', 'carolw', 'carol.white@example.com'),
-('David Brown', 'davidb', 'david.brown@example.com'),
-('Marchel Lee', 'marchell', 'mar.lee@example.com'),
-('Eve Davis', 'eved', 'eve.davis@example.com');
+('Naruto Uzumaki', 'naruto', 'naruto.uzumaki@example.com'),
+('Sakura Haruno', 'sakura', 'sakura.haruno@example.com'),
+('Luffy Monkey', 'luffy', 'luffy.monkey@example.com'),
+('Goku Son', 'goku', 'goku.son@example.com'),
+('Edward Elric', 'edward', 'edward.elric@example.com'),
+('Natsu Dragneel', 'natsu', 'natsu.dragneel@example.com'),
+('Light Yagami', 'light', 'light.yagami@example.com'),
+('Inuyasha', 'inuyasha', 'inuyasha@example.com'),
+('Mikasa Ackerman', 'mikasa', 'mikasa.ackerman@example.com');
