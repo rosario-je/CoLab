d21a3799d836b85e962162d3e26399f2dc1ab8de
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-25 01:14:21 -0400

    refactor: Update SearchBar component markup for better accessibility and semantics

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 export const SearchBar = () => {
   return (
-    <label className="input flex items-center gap-2 w-full bg-menu-colors rounded-none">
-      <i className="fa-solid fa-magnifying-glass mx-3 text-[#000000]"></i>
-      <input type="text" className="grow rounded-none" placeholder="Search" />
-      <i class="fa-solid fa-bars mr-10"></i>
-    </label>
+    <div>
+      <label className="input flex items-center gap-2 w-full bg-menu-colors rounded-none">
+        <i className="fa-solid fa-magnifying-glass mx-3 text-[#000000]"></i>
+        <input type="text" className="grow rounded-none" placeholder="Search" />
+        <i className="fa-solid fa-bars mr-10"></i>
+      </label>
+    </div>
   );
 };
