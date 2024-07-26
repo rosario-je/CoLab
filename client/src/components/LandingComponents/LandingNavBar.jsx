c508721b4695872ea533faab3482af6cc7924f93
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-25 23:04:03 -0400

    refactor: Update image import path in LandingNavBar component

--- client/src/components/LandingComponents/LandingNavBar.jsx
+++ client/src/components/LandingComponents/LandingNavBar.jsx
@@ -5,16 +5,18 @@ import {
   SignInButton,
   UserButton,
 } from "@clerk/clerk-react";
-import Vial from "../images/vial.png";
+import vial from "../../images/vial.png";
+import { useNavigate } from "react-router-dom";
 
-export const LandingNavBar = ({ handleCoLabHome}) => {
+export const LandingNavBar = ({ handleCoLabHome }) => {
+  const navigate = useNavigate();
   return (
     <div className="fixed top-0 left-0 right-0 z-50">
       <div className="navbar bg-landing-navbar-color/50 backdrop-blur-xl">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl text-white group" onClick={handleCoLabHome}>
+          <a className="btn btn-ghost text-xl text-white group">
             <img
-              src={Vial}
+              src={vial}
               className="h-7 mb-2 group-hover:animate-unstableBeaker"
             />
             CoLab
