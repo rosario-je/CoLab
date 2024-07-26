d243b20bdd8f0730cbfe84e77a8a7a7fa2cc83bf
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-25 21:11:52 -0400

    refactor: Add ProjectPageAvatars component for displaying avatars on project page

---
+++ client/src/components/GroupProjectPage/ProjectPageAvatars.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const ProjectPageAvatars = () => {
+  return (
+    <div>
+      <div className="avatar">
+        <div className="w-16 rounded-full border-blue-400 border-4">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+      </div>
+    </div>
+  );
+};
