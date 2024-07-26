0b7fa418ba97a0bf47b479f4819f7808c76290d2
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-25 23:03:41 -0400

    refactor: Update SearchBar component to fix input alignment issue

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const SearchBar = () => {
   return (
     <div>
-      <label className="input flex items-center gap-2 w-full bg-menu-colors rounded-none">
+      <label className="input fixed  flex items-center gap-2 w-full bg-menu-colors rounded-none">
         <i className="fa-solid fa-magnifying-glass mx-3 text-[#000000]"></i>
         <input type="text" className="grow rounded-none" placeholder="Search" />
         <i className="fa-solid fa-bars mr-10"></i>
