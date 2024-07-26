92f6ca8b69f2999a4eac8285dfb2870710122ab2
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-25 23:03:36 -0400

    refactor: Update Navbar component to use react-router-dom for navigation

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import Vial from "../images/vial.png";
 import { useNavigate } from "react-router-dom";
 
-export const Navbar = ({ handleCoLabHome}) => {
+export const Navbar = () => {
   const navigate = useNavigate();
-  const handleNavigateCreateProject = () => {
-    navigate("/project/create");
-  };
 
   return (
     <div className="navbar w-screen bg-navbar-color text-text-color fixed top-0 left-0 right-0 z-50 mb-6">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl group" onClick={handleCoLabHome}>
+        <a
+          className="btn btn-ghost text-xl group"
+          onClick={() => {
+            navigate("/dashboard");
+          }}
+        >
           <img
             src={Vial}
             className="h-7 mb-2 group-hover:animate-unstableBeaker"
@@ -19,8 +21,13 @@ export const Navbar = ({ handleCoLabHome}) => {
           CoLab
         </a>
       </div>
-      <div className="flex-none">
-        <button className="btn btn-square btn-ghost w-auto px-2 mx-1 group" onClick={handleNavigateCreateProject}>
+      <div className="pr-5">
+        <button
+          className="btn btn-square btn-ghost w-auto px-2 mx-1 group"
+          onClick={() => {
+            navigate("/project/create");
+          }}
+        >
           <i className="fa-solid fa-circle-plus group-hover:animate-bounceFast"></i>{" "}
           Create a project
         </button>
