d65de72885f42fbaeb27fed07917b91fcc0be76d
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-25 21:12:26 -0400

    refactor: Update ProjectPage component to include navigation, left menu, and right menu

--- client/src/pages/ProjectPage.jsx
+++ client/src/pages/ProjectPage.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+// Dashboard.jsx
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUser } from "@clerk/clerk-react";
+
+import { Navbar } from "../components/Navbar";
+import { UserLeftMenu } from "../components/UserLeftMenu";
+import { ProjectPageDetails } from "../components/GroupProjectPage/ProjectPageDetails";
+import { ProjectRightMenu } from "../components/GroupProjectPage/ProjectRightMenu";
 
 export const ProjectPage = () => {
+  const { isSignedIn } = useUser();
+  const navigate = useNavigate();
+
   return (
-    <div>ProjectPage</div>
-  )
-}
+    <div className="flex flex-col h-screen">
+      <Navbar />
+      <div className="flex flex-1 mt-16">
+        <UserLeftMenu />
+        <div className="flex grow  justify-center overflow-y-auto">
+          <ProjectPageDetails />
+        </div>
+        <ProjectRightMenu />
+      </div>
+    </div>
+  );
+};
