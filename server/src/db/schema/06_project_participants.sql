0029d60044b1f594b5b8c860d9c4e20697bd30f6
Rebecca <rebecca90469@gmail.com>
2024-07-26 14:09:29 -0400

    Update to user's table and mock data

--- server/src/db/schema/06_project_participants.sql
+++ server/src/db/schema/06_project_participants.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS project_participants CASCADE;
 CREATE TABLE project_participants (
   id SERIAL PRIMARY KEY,
   project_id INT REFERENCES projects(id) ON DELETE CASCADE,
-  participant_id INT REFERENCES temp_users(id) ON DELETE CASCADE,
+  participant_id INT REFERENCES users(id) ON DELETE CASCADE,
   is_approved BOOLEAN DEFAULT FALSE,
   is_pending_join BOOLEAN DEFAULT FALSE
 );
---
+++ server/src/db/seeds/11_users_seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO users (first_name, last_name, username, password, email, profile_pic) VALUES
+('Naruto','Uzumaki', 'naruto', 'password', 'naruto.uzumaki', 'naruto.uzumaki@example.com','user_1053244.png'),
+('Sakura','Haruno', 'sakura', 'password', 'sakura.haruno', 'sakura.haruno@example.com', 'user_1053244.png'),
+('Luffy','Monkey', 'luffy', 'password', 'luffy.monkey', 'luffy.monkey@example.com', 'user_1053244.png'),
+('Goku','Son', 'goku', 'password', 'goku.son', 'goku.son@example.com', 'user_1053244.png'),
+('Edward','Elric', 'edward', 'password', 'edward.elric', 'edward.elric@example.com', 'user_1053244.png'),
+('Natsu','Dragneel', 'natsu', 'password', 'natsu.dragneel', 'natsu.dragneel@example.com', 'user_1053244.png'),
+('Light','Yagami', 'light', 'password', 'light.yagami', 'light.yagami@example.com', 'user_1053244.png'),
+('Inu','yasha', 'inuyasha', 'password', 'inuyasha', 'inuyasha@example.com', 'user_1053244.png'),
+('Mikasa','Ackerman', 'mikasa', 'password', 'mikasa.ackerman', 'mikasa.ackerman@example.com', 'user_1053244.png');
