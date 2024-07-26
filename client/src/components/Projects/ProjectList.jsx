4f7233cef55b07abf21ab13f50837b0292913b82
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-25 23:04:20 -0400

    refactor: Populate ProjectList component with data from the backend

--- client/src/components/Projects/ProjectList.jsx
+++ client/src/components/Projects/ProjectList.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ProjectCard } from './ProjectCard'
 
 export const ProjectList = () => {
+  //will be populated with data from the backend
   return (
     <div className='w-11/12 '>
       <ProjectCard />
