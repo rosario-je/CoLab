0c73d9ce7d4a034432a2db0b0299d78bd5df66e3
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-26 14:34:32 -0400

    refactor: Update SearchBar component styling and structure

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const SearchBar = () => {
   return (
-    <div>
-      <label className="input fixed  flex items-center gap-2 w-full bg-menu-colors rounded-none">
+    <div className="search-bar flex justify-center">
+      <label className="input flex  mx-auto fixed items-center gap-2 w-full bg-menu-colors rounded-none">
         <i className="fa-solid fa-magnifying-glass mx-3 text-[#000000]"></i>
         <input type="text" className="grow rounded-none" placeholder="Search" />
         <i className="fa-solid fa-bars mr-10"></i>
