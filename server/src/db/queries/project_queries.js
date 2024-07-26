a38438adc4d8f352985a593b3192cdfc28af6834
Rebecca <rebecca90469@gmail.com>
2024-07-26 19:57:40 -0400

    Setup for populating user dash for projects owned by a specific user or a project they are apart of

--- server/src/db/queries/project_queries.js
+++ server/src/db/queries/project_queries.js
@@ -4,28 +4,28 @@ const getAllProjects = async () => {
   try {
     const data = await db.query(
       `SELECT 
-    p.id AS project_id,
-    p.name,
-    p.description,
-    p.owner_id,
-    p.max_participants,
-    p.github_repo,
-    p.created_at,
-    p.is_accepting_users,
-    p.is_in_progress,
-    ARRAY_AGG(DISTINCT par.participant_id) AS projects_participants,
-    ARRAY_AGG(DISTINCT pic.picture_path) AS projects_pics,
-    ARRAY_AGG(DISTINCT tech.tech_name) AS tech_requirements
-    FROM 
-    projects p
-    LEFT JOIN 
-    projects_participants par ON p.id = par.project_id
-    LEFT JOIN 
-    projects_pics pic ON p.id = pic.project_id
-    LEFT JOIN 
-    tech_requirements tech ON p.id = tech.project_id
-    GROUP BY 
-    p.id;`
+        p.id AS project_id,
+        p.name,
+        p.description,
+        p.owner_id,
+        p.max_participants,
+        p.github_repo,
+        p.created_at,
+        p.is_accepting_users,
+        p.is_in_progress,
+      ARRAY_AGG(DISTINCT par.participant_id) AS projects_participants,
+      ARRAY_AGG(DISTINCT pic.picture_path) AS projects_pics,
+      ARRAY_AGG(DISTINCT tech.tech_name) AS tech_requirements
+      FROM 
+        projects p
+      LEFT JOIN 
+        projects_participants par ON p.id = par.project_id
+      LEFT JOIN 
+        projects_pics pic ON p.id = pic.project_id
+      LEFT JOIN 
+        tech_requirements tech ON p.id = tech.project_id
+      GROUP BY 
+        p.id;`
     );
     return data.rows;
   } catch (error) {
@@ -33,40 +33,101 @@ const getAllProjects = async () => {
   }
 };
 
+const getProjectsOwnedByMe = async (user_id) => {
+  try {
+    const data = await db.query(
+      `SELECT 
+      p.id AS project_id,
+      p.name,
+      p.description,
+      p.owner_id,
+      p.max_participants,
+      p.github_repo,
+      p.created_at,
+      p.is_accepting_users,
+      p.is_in_progress,
+    ARRAY_AGG(DISTINCT par.participant_id) AS projects_participants,
+    ARRAY_AGG(DISTINCT pic.picture_path) AS projects_pics,
+    ARRAY_AGG(DISTINCT tech.tech_name) AS tech_requirements
+    FROM 
+      projects p
+    LEFT JOIN 
+      projects_participants par ON p.id = par.project_id
+    LEFT JOIN 
+      projects_pics pic ON p.id = pic.project_id
+    LEFT JOIN 
+      tech_requirements tech ON p.id = tech.project_id
+    WHERE p.owner_id = $1
+    GROUP BY 
+      p.id;`,
+      [user_id]
+    );
+    return data.rows;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+const getProjectsIdsIAmIn = async (user_id) => {
+  try {
+    const data = await db.query(
+      `SELECT 
+      p.id AS project_id
+    FROM 
+      projects p
+    LEFT JOIN 
+      projects_participants par ON p.id = par.project_id
+    LEFT JOIN 
+      projects_pics pic ON p.id = pic.project_id
+    LEFT JOIN 
+      tech_requirements tech ON p.id = tech.project_id
+    WHERE par.participant_id = $1
+    GROUP BY 
+      p.id;`,
+      [user_id]
+    );
+    return data.rows;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// This will be the improved version of the getAllProjects function
-// const getAllProjects = async (user_id) => {
-//   try {
-//     const data = await db.query(
-//       `SELECT * FROM projects
-//       WHERE NOT user_id = $1
-//       ORDER BY projects.created_at ASC`,
-//       [user_id]
-//     );
-//     return data.rows;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-//This will be to get the projects that a logged in user has created
-// const getMyProjects = async (user_id) => {
-//   try{
-//     const data = await db.query(
-//       `SELECT * FROM projects
-//       WHERE user_id = $1
-//       ORDER BY projects.created_at ASC`,
-//       [user_id]
-//     );
-//     return data.rows;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+const getProjectsIAmInById = async (project_ids) => { 
+  try {
+    const data = await db.query(
+    `SELECT 
+      p.id AS project_id,
+      p.name,
+      p.description,
+      p.owner_id,
+      p.max_participants,
+      p.github_repo,
+      p.created_at,
+      p.is_accepting_users,
+      p.is_in_progress,
+    ARRAY_AGG(DISTINCT par.participant_id) AS projects_participants,
+    ARRAY_AGG(DISTINCT pic.picture_path) AS projects_pics,
+    ARRAY_AGG(DISTINCT tech.tech_name) AS tech_requirements
+    FROM 
+      projects p
+    LEFT JOIN 
+      projects_participants par ON p.id = par.project_id
+    LEFT JOIN 
+      projects_pics pic ON p.id = pic.project_id
+    LEFT JOIN 
+      tech_requirements tech ON p.id = tech.project_id
+    WHERE p.id = ANY($1)
+    GROUP BY 
+      p.id;`,
+      [project_ids]
+    );
+    return data.rows;
+  } catch (error) {
+    console.log(error);
+  };
+};
 
 // This will be for creating a new project
-
 const createNewProject = async (name, description, user_id, max_participants, github_repo) => {
   try {
     const data = await db.query(
@@ -81,6 +142,7 @@ const createNewProject = async (name, description, user_id, max_participants, gi
   }
 };
 
+// This will be for getting a single project page
 const getProjectPage = async (project_id) => {
   try {
     const data = await db.query(
@@ -128,4 +190,4 @@ const getProjectPage = async (project_id) => {
 //   }
 // };
 
-export { getAllProjects, createNewProject, getProjectPage };
+export { getAllProjects, createNewProject, getProjectPage, getProjectsIdsIAmIn, getProjectsIAmInById, getProjectsOwnedByMe };
