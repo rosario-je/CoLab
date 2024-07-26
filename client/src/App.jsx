c145ccf0c7bc3b9362cefaad48a6e65ed667adff
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-25 21:11:08 -0400

    refactor: Update routing paths for protected routes in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,20 @@
 // App.jsx
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { Landing } from "./pages/Landing";
 import { Dashboard } from "./pages/Dashboard";
 import { ProjectPage } from "./pages/ProjectPage";
 import { CreateProject } from "./pages/CreateProject";
-import ProtectedRoute from "./components/helper_component/ProtectedRoute";
 
 function App() {
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route
-          path="/dashboard"
-          element={<ProtectedRoute element={<Dashboard />} />}
-        />
-        <Route
-          path="/project/:id"
-          element={<ProtectedRoute element={<ProjectPage />} />}
-        />
-        <Route
-          path="/project/create"
-          element={<ProtectedRoute element={<CreateProject />} />}
-        />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/project/:id" element={<ProjectPage />} />
+        <Route path="/project/create" element={<CreateProject />} />
       </Routes>
     </div>
   );
