09ea6b1c385fbf6ad10c17fcb83b6a997a0012b9
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-25 23:03:28 -0400

    refactor: Update CreateProjectNavbar component to use react-router-dom for navigation

--- client/src/components/CreateProjectNavbar.jsx
+++ client/src/components/CreateProjectNavbar.jsx
@@ -1,24 +1,18 @@
 import React from "react";
 import Vial from "../images/vial.png";
+import { useNavigate } from "react-router-dom";
 
-export const CreateProjectNavbar = ({ handleCoLabHome}) => {
-
+export const CreateProjectNavbar = () => {
+  const navigate = useNavigate();
   return (
-    <div className="navbar w-screen bg-navbar-color text-text-color fixed top-0 left-0 right-0 z-50 mb-6">
+    <div className="navbar w-screen bg-navbar-color text-text-color fixed top-0 left-0 right-0 z-50 mb-6 pr-5">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl group" onClick={handleCoLabHome}>
-          <img
-            src={Vial}
-            className="h-7 mb-2 group-hover:animate-unstableBeaker"
-          />
+        <a className="btn btn-ghost text-xl group" onClick={() => {navigate("/dashboard")}}>
+          <img src={Vial} className="h-7 mb-2 group-hover:animate-unstableBeaker"/>
           CoLab
         </a>
       </div>
-      <div className="flex-none">
-        <button className="btn btn-square btn-ghost w-auto px-2 mx-1 group">
-          <i className="fa-solid fa-comments group-hover:animate-sideToSide"></i>{" "}
-          Messages
-        </button>
+      <div className="right-navbar-button">
         <button className="btn btn-square btn-ghost w-auto px-2 mx-1 group">
           <i className="fa-solid fa-bell group-hover:animate-swing"></i> Join
           requests
