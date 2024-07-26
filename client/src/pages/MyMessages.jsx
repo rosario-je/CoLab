480b45acbf54295ec68ef0ccc18eb895b0727e98
Tommy <tommy.azran@gmail.com>
2024-07-26 17:59:46 -0400

    refactor: Update page title in MyMessages component

--- client/src/pages/MyMessages.jsx
+++ client/src/pages/MyMessages.jsx
@@ -13,7 +13,7 @@ export const MyMessages = () => {
       <div className="flex flex-col w-full h-full bg-project-background overflow-hidden">
         <div className="flex-grow flex flex-col justify-center items-center h-full">
           <div className="w-full flex justify-start items-center py-1 bg-menu-colors h-[75px]">
-          <h1 className="text-white text-2xl">My Project Requests</h1>
+          <h1 className="text-white text-2xl">My Messages</h1>
           </div>
           <MyMessageList />
         </div>
