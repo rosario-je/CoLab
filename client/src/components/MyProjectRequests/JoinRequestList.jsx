91825981e92d21a19d50d931c18a2fe3a94a1a82
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-27 16:42:31 -0400

    refactor: Update MyProjectRequests page layout and structure

--- client/src/components/MyProjectRequests/JoinRequestList.jsx
+++ client/src/components/MyProjectRequests/JoinRequestList.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { JoinRequestListItems } from "./JoinRequestListItems";
 
-
 export const JoinRequestList = () => {
   return (
     <div className="my-join-requests h-full w-auto flex flex-col items-center mx-72">
       {/* map through the component when database is populated */}
-      <h1 className="text-white text-2xl">My Project Requests</h1>
-      <JoinRequestListItems />
-      <JoinRequestListItems />
-      <JoinRequestListItems />
-      <JoinRequestListItems />
+      <div className="bg-menu-colors p-5 fixed w-full z-10 mx-auto">
+        <h1 className="text-white text-2xl mx-72">My Project Requests</h1>
+      </div>
+      <div className="flex flex-col w-full items-center mt-16">
+        <JoinRequestListItems />
+        <JoinRequestListItems />
+        <JoinRequestListItems />
+        <JoinRequestListItems />
+      </div>
     </div>
   );
 };
