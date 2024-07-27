ea4dbaab472bcc54c7e43f64a06f40fec64d0e8d
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-27 16:33:59 -0400

    refactor: Add JoinRequestListItems component for displaying join request list items in MyProjectRequests page

---
+++ client/src/components/MyProjectRequests/JoinRequestListItems.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const JoinRequestListItems = () => {
+  return (
+    <>
+      <div className="card bg-neutral text-neutral-content w-11/12 my-10">
+        <div className="card-body items-center text-center flex flex-col justify-center gap-[20px]">
+          <h2 className="card-title self-start text-2xl">Jack Sparrow</h2>
+          <p className="self-start">Has requested to join:</p>
+          <div className="card-actions w-full justify-between self-end">
+            <h2 className="text-2xl">CoLab</h2>
+            <div className="flex justify-center items-center gap-[15px]">
+              <button className="btn bg-green hover:bg-dark-green text-white">
+                Accept
+              </button>
+              <button className="btn bg-red hover:bg-dark-red text-white">
+                Deny
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
