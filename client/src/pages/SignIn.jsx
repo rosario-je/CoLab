84986a270837bbba686eaed2adf4e31b185a3947
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-26 20:57:57 -0400

    refactor: Add SignIn and SignUp pages for user authentication

---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+
+export const SignIn = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6 gap-y-4">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+            <div className="flex mt-6 gap-y-4 justify-center pb-8 ">
+              <button
+                onClick={() => navigate("/signup")}
+                className="btn btn-primary w-80"
+              >
+                Don't have an account?
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
