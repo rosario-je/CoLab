63fbcaf00f2caa518e9e0eb58ffc1827920b167b
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-27 20:10:44 -0400

    refactor: Update CreateProject page to include currentUser prop in UserLeftMenu component

--- client/src/pages/CreateProject.jsx
+++ client/src/pages/CreateProject.jsx
@@ -8,19 +8,14 @@ import { UserRightMenu } from "../components/UserRightMenu";
 import { ProjectCreateField } from "../components/Projects/ProjectCreateField";
 import { CreateProjectTechStackModal } from "../components/CreateProjectTechStackModal";
 
-export const CreateProject = ({
-  handleCoLabHome,
-  handleTechStacksModal,
-  techModal,
-}) => {
+export const CreateProject = ({ handleCoLabHome, handleTechStacksModal, techModal, currentUser, handleLogout }) => {
   const navigate = useNavigate();
-
 
   return (
     <div className="flex flex-col h-screen mx-72">
       <CreateProjectNavbar handleCoLabHome={handleCoLabHome} />
       <div className="flex flex-1 mt-16">
-        <UserLeftMenu />
+        <UserLeftMenu currentUser={currentUser}/>
         <div className="flex flex-col w-full h-full bg-project-background overflow-hidden">
           <div className="flex-grow flex justify-center h-full mx-10 my-5">
             {techModal === true && (
