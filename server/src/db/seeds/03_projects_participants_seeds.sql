037adea7dbc6b8ff4dd376bc320b93bccf326166
Rebecca <rebecca90469@gmail.com>
2024-07-28 22:52:57 -0400

    Final seeds update

--- server/src/db/seeds/03_projects_participants_seeds.sql
+++ server/src/db/seeds/03_projects_participants_seeds.sql
@@ -3,6 +3,7 @@ INSERT INTO projects_participants (project_id, participant_id) VALUES
 (1, 2),  -- Sakura
 (1, 3),  -- Luffy
 (1, 10), -- Zenitsu
+(1, 13), -- Megumi
 
 -- Naruto Missions Tracker (Owner: Sakura, Max: 4)
 (2, 1),  -- Naruto
@@ -28,7 +29,7 @@ INSERT INTO projects_participants (project_id, participant_id) VALUES
 (6, 1),  -- Naruto
 (6, 2),  -- Sakura
 
--- Fairy Tail Guild Manager (Owner: Sakura, Max: 4)
+-- Fairy Tail Guild Manager (Owner: Natsu, Max: 4)
 (7, 1),  -- Naruto
 (7, 3),  -- Luffy
 (7, 11), -- Gojo
@@ -40,13 +41,27 @@ INSERT INTO projects_participants (project_id, participant_id) VALUES
 (9, 1),  -- Naruto
 (9, 2),  -- Sakura
 
--- Dragon Ball Tournament Organizer (Owner: Goku, Max: 4) -- AT MAX CAPACITY
+-- Attack on Titan Defense Simulator (Owner: Mikasa, Max: 4) 
+(10, 4), -- Goku
 (10, 6), -- Natsu
 (10, 13), -- Megumi
-(10, 14), -- Reigen
-(10, 2), -- Sakura
+(10, 11), -- Gojo
 
--- Saiyan Training Log (Owner: Goku, Max: 4) 
+-- Dragon Ball Tournament Organizer (Owner: Goku, Max: 4) -- AT MAX CAPACITY
 (11, 6), -- Natsu
 (11, 13), -- Megumi
-(11, 14); -- Reigen
+(11, 14), -- Reigen
+(11, 2), -- Sakura
+
+-- Saiyan Training Log (Owner: Goku, Max: 4) 
+(12, 9); -- Mikasa
+(12, 13), -- Megumi
+(12, 14); -- Reigen
+
+-- Thunder Breathing Training Hub (Owner: Zenitsu, Max: 4) 
+(13, 9); -- Mikasa
+
+-- Saiyan Battle Simulator (Owner: Goku, Max: 4)
+(14, 6), -- Natsu
+(14, 13), -- Megumi
+(14, 14); -- Reigen
