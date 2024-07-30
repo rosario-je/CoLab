fcf9716b86a4d7e53e3388bce82964fef2f946d6
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-29 22:17:28 -0400

    refactor: Add lazy loading for images in MyProjects component

--- client/src/pages/MyProjects.jsx
+++ client/src/pages/MyProjects.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Navbar } from "../components/Navbar";
 import { UserLeftMenu } from "../components/UserLeftMenu";
@@ -7,51 +8,33 @@ import { UserRightMenu } from "../components/UserRightMenu";
 export const MyProjects = ({ handleCoLabHome, currentUser, handleLogout }) => {
   const navigate = useNavigate();
   const [activeButton, setActiveButton] = useState(null);
+  const userId = currentUser.id;
 
   const handleButtonClick = (button) => {
     setActiveButton(button);
   };
 
+  useEffect(() => {
+    const fetchUserProjects = async () => {
+      try {
+        const userProjectData = await axios.get("/api/dashboard/projects");
+      } catch (error) {
+        console.error("Error in getting user projects: ", error.message);
+      }
+    }
+    fetchUserProjects();
+  }, [])
+
   return (
     <div className="flex flex-col h-screen">
+
       <Navbar handleCoLabHome={handleCoLabHome} currentUser={currentUser}/>
+      
       <div className="flex flex-1 mt-16">
         <UserLeftMenu currentUser={currentUser}/>
+      
         <div className="flex flex-col w-full bg-grey overflow-hidden mx-72">
-          <div className="flex justify-center items-center w-[100%] bg-menu-colors py-3 px-2 h-16">
-            <div className="flex justify-around items-center bg-projects-bar rounded-[10px] w-[90%] py-1 w-full]">
-              <button
-                onClick={() => handleButtonClick("allProjects")}
-                className={`btn text-white w-60 ${
-                  activeButton === "allProjects"
-                    ? "bg-projects-bar-button"
-                    : "bg-default-color"
-                }`}
-              >
-                All Projects
-              </button>
-              <button
-                onClick={() => handleButtonClick("myProjects")}
-                className={`btn text-white w-60 ${
-                  activeButton === "myProjects"
-                    ? "bg-projects-bar-button"
-                    : "bg-default-color"
-                }`}
-              >
-                My Projects
-              </button>
-              <button
-                onClick={() => handleButtonClick("collaborations")}
-                className={`btn text-white w-60 ${
-                  activeButton === "collaborations"
-                    ? "bg-projects-bar-button"
-                    : "bg-default-color"
-                }`}
-              >
-                Collaborations
-              </button>
-            </div>
-          </div>
+
         </div>
         <UserRightMenu currentUser={currentUser}/>
       </div>
