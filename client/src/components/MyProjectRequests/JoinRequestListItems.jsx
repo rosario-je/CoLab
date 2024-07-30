d89c414b0c2e58162a12b2ddc2979d50e00fd4c1
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-29 22:16:48 -0400

    refactor: Update JoinRequestListItems component to improve user information display

--- client/src/components/MyProjectRequests/JoinRequestListItems.jsx
+++ client/src/components/MyProjectRequests/JoinRequestListItems.jsx
@@ -5,14 +5,21 @@ export const JoinRequestListItems = () => {
     <>
       <div className="card bg-neutral text-neutral-content w-11/12 my-10">
         <div className="card-body items-center text-center flex flex-col justify-center gap-[20px]">
-          <h2 className="card-title self-start text-2xl">Jack Sparrow</h2>
-          <p className="self-start">Has requested to join:</p>
+          <h2 className="requested-user self-start text-3xl font-semibold">
+            Jack Sparrow
+          </h2>
+
+          <p className="self-start font-light text-3xl italic">
+            Has requested to join:
+          </p>
+
           <div className="card-actions w-full justify-between self-end">
-            <h2 className="text-2xl">CoLab</h2>
+            <h2 className="text-5xl font-bold">CoLab</h2>
             <div className="flex justify-center items-center gap-[15px]">
               <button className="btn bg-green hover:bg-dark-green text-white">
                 Accept
               </button>
+
               <button className="btn bg-red hover:bg-dark-red text-white">
                 Deny
               </button>
