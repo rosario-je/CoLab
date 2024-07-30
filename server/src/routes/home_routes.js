413b8feb9e828a7dab25739b0206cfbac6622db4
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-30 01:33:27 -0400

    refactor: Update UserRightMenu import in MyProjects component

--- server/src/routes/home_routes.js
+++ server/src/routes/home_routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getAllProjects, getProjectsOwnedByMe, getProjectsIAmInById, getProjectsIdsIAmIn } from '../db/queries/project_queries.js';
-import { getAllJoinRequests, addUserToProject, approveJoinRequest,  } from '../db/queries/user_queries.js';
+import { getAllJoinRequests, addUserToProject, approveJoinRequest, } from '../db/queries/user_queries.js';
 const router = express.Router();
 
 // http://localhost:5000/api/dashboard/projects
@@ -15,14 +15,14 @@ router.get('/projects', async (req, res) => {
 });
 
 // http://localhost:5000/api/dashboard/my_projects
-router.get('/my_projects', async (req, res) => {
+router.get('/:id/my_projects', async (req, res) => {
   const { id: user_id } = req.session.user;
   try {
     const myOwnedProjects = await getProjectsOwnedByMe(user_id);
     const myJoinedProjectsIdArray = await getProjectsIdsIAmIn(user_id);
     const projectsIdArray = myJoinedProjectsIdArray.map(project => project.project_id);
     const myJoinedProjects = await getProjectsIAmInById(projectsIdArray);
-  return res.status(200).json(myOwnedProjects.concat(myJoinedProjects));
+    return res.status(200).json(myOwnedProjects.concat(myJoinedProjects));
   } catch (error) {
     console.error("Error in getting user projects: ", error.message);
     res.status(500).json({ error: "Internal server error" });
