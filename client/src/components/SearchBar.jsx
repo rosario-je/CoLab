1fe4e16fdfdfe0d1a0ad1a707e430d65f85fea68
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-29 22:15:48 -0400

    refactor: Adjust height of search bar in SearchBar component

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const SearchBar = () => {
   return (
     <div className="search-bar flex justify-center">
-      <label className="input flex  mx-auto fixed items-center gap-2 w-full bg-menu-colors rounded-none">
+      <label className="input flex  mx-auto fixed items-center gap-2 w-full bg-menu-colors rounded-none h-[65px]">
         <i className="fa-solid fa-magnifying-glass mx-3 text-[#000000]"></i>
         <input type="text" className="grow rounded-none" placeholder="Search" />
         <i className="fa-solid fa-bars mr-10"></i>
