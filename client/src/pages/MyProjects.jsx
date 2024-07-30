d5358fc34f95044cfd64b6d0435cac2e6f9f4288
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-30 01:33:22 -0400

    refactor: Update UserRightMenu import in MyProjects component

--- client/src/pages/MyProjects.jsx
+++ client/src/pages/MyProjects.jsx
@@ -1,42 +1,57 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+
 import { Navbar } from "../components/Navbar";
 import { UserLeftMenu } from "../components/UserLeftMenu";
-import { UserRightMenu } from "../components/UserRightMenu";
+import { UserRightMenu } from "../components//UserRightMenuComponents/UserRightMenu";
+import { ProjectCard } from "../components/Projects/ProjectCard";
 
-export const MyProjects = ({ handleCoLabHome, currentUser, handleLogout }) => {
-  const navigate = useNavigate();
+export const MyProjects = ({ handleCoLabHome, currentUser}) => {
+  const [projects, setProjects] = useState([]);
   const [activeButton, setActiveButton] = useState(null);
   const userId = currentUser.id;
-
-  const handleButtonClick = (button) => {
-    setActiveButton(button);
-  };
+  
+  // const handleButtonClick = (button) => {
+  //   setActiveButton(button);
+  // };
 
   useEffect(() => {
     const fetchUserProjects = async () => {
       try {
-        const userProjectData = await axios.get("/api/dashboard/projects");
+        const userProjectData = await axios.get(`/api/dashboard/${userId}/my_projects`);
+        setProjects(userProjectData.data);
+        console.log("User Projects: ", userProjectData.data);
       } catch (error) {
         console.error("Error in getting user projects: ", error.message);
       }
-    }
+    };
     fetchUserProjects();
-  }, [])
-
+  }, []);
+console.log(projects);
   return (
     <div className="flex flex-col h-screen">
+      <Navbar handleCoLabHome={handleCoLabHome} currentUser={currentUser} />
 
-      <Navbar handleCoLabHome={handleCoLabHome} currentUser={currentUser}/>
-      
       <div className="flex flex-1 mt-16">
-        <UserLeftMenu currentUser={currentUser}/>
-      
-        <div className="flex flex-col w-full bg-grey overflow-hidden mx-72">
+        <UserLeftMenu currentUser={currentUser} />
 
+        <div className="flex flex-col w-full bg-grey overflow-hidden mx-72 px-10">
+          {projects.map((project) => (
+            <ProjectCard
+              key={project.project_id}
+              name={project.name}
+              owner={project.owner_id}
+              description={project.description}
+              participants={project.participants}
+              techStack={project.tech_requirements}
+              acceptingUsers={project.is_accepting_users}
+              maxParticipants={project.max_participants}
+              page="myprojects"
+              currentUserId = {userId}
+            />
+          ))}
         </div>
-        <UserRightMenu currentUser={currentUser}/>
+        <UserRightMenu currentUser={currentUser} />
       </div>
     </div>
   );
