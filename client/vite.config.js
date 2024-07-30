01c1dc98b01569540c535e4bc1af4f0f35b0d255
Tommy <tommy.azran@gmail.com>
2024-07-30 13:58:51 -0400

    refactor: Update ProjectCreateField component UI and layout

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,28 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//       }
+//     }
+//   }
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
+
+export default defineConfig({ 
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      }
-    }
-  }
+      "/api": "http://localhost:8080",
+    }  
+  } ,
+  plugins: [react()],
 })
--- server/src/server_index.js
+++ server/src/server_index.js
@@ -1,49 +1,44 @@
 import "dotenv/config.js";
-import express from 'express';
-import { config } from 'dotenv';
-import session from 'express-session';
-import morgan from 'morgan';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import express from "express";
+import { config } from "dotenv";
 
-import chatsRoutes from './routes/chats_routes.js';
-import projectsRoutes from './routes/projects_routes.js';
-import homeRoutes from './routes/home_routes.js';
-import userRoutes from './routes/user_routes.js';
-import db from './db/connection.js';
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import chatsRoutes from "./routes/chats_routes.js";
+import projectsRoutes from "./routes/projects_routes.js";
+import homeRoutes from "./routes/home_routes.js";
+import userRoutes from "./routes/user_routes.js";
+import db from "./db/connection.js";
 
 config();
 
-
 const PORT = process.env.PORT || 5000;
 const app = express();
+app.use(cors());
+app.options('*', cors())
 app.use(express.json());
-app.use(morgan('dev'));
 app.use(cookieParser());
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true // Allow cookies to be sent
-}));
 
 // Middleware for Auth, session secret should be in .env file
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: process.env.NODE_ENV === 'production' }
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: process.env.NODE_ENV === "production" },
+  })
+);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to CoLab!');
+app.get("/", (req, res) => {
+  res.send("Welcome to CoLab!");
 });
 
-app.use("/chats", chatsRoutes)
-app.use("/projects", projectsRoutes)
-app.use("/api/dashboard", homeRoutes)
-app.use("/api", userRoutes)
-
-
+app.use("/chats", chatsRoutes);
+app.use("/projects", projectsRoutes);
+app.use("/api/dashboard", homeRoutes);
+app.use("/api", userRoutes);
 
 app.listen(PORT, () => {
-  console.log('Server is running on port', PORT);
+  console.log("Server is running on port", PORT);
 });
