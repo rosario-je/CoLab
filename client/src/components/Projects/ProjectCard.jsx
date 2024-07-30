b3b4e00a1bf31b33a35f52b1024c6e5ec1ebca00
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-29 22:16:52 -0400

    refactor: Update ProjectCard component to improve layout and display project details

--- client/src/components/Projects/ProjectCard.jsx
+++ client/src/components/Projects/ProjectCard.jsx
@@ -1,106 +1,49 @@
 import React from "react";
+import { ProjectUserAvatar } from "./ProjectUserAvatar";
+import { ProjectTechStack } from "./ProjectTechStack";
 
 export const ProjectCard = (props) => {
-  const { participants } = props;
+  const { name, description, participants, techStack, acceptingUsers } = props;
 
   return (
     <>
-      <div className="card bg-base-100 w-full shadow-xl border-solid border-2 border-website-purple/25 text-white my-16">
+      <div className="card bg-base-100 w-full shadow-xl border-solid border-2 border-website-purple/25 text-white my-8">
         <div className="card-body h-96">
           <div className="flex justify-between items-center text-white">
-            <div className="flex">
-              <i className="fa-regular fa-square text-5xl mr-2"></i>
-              <h2 className="card-title font-bold text-4xl">{props.name}</h2>
+            <div className="project-details-1 flex space-x-6">
+              <div className="w-28 h-28 bg-white rounded-3xl" />
+              <div className="flex flex-col justify-center">
+                <h2 className="card-title font-bold text-4xl">{name}</h2>
+                <h3>@Project Owner</h3>
+              </div>
             </div>
             <div className="avatar-group -space-x-6 rtl:space-x-reverse">
               {participants.map((participant) => {
                 return (
-                  <div className="avatar" key={participant.participant_id}>
-                    <div className="w-12">
-                      <img src={participant.participant_pic} />
-                    </div>
-                  </div>
+                  <ProjectUserAvatar
+                    key={participant.participant_id}
+                    participant={participant}
+                  />
                 );
               })}
             </div>
           </div>
-          <p className="font-light text-xl pt-5">{props.description}</p>
+          <p className="font-light text-xl pt-5">{description}</p>
           <div className="flex card-actions w-full justify-between items-center ">
             <div className="w-[60%]">
-              <div className="badge bg-website-purple text-white px-5 py-4 mx-1 w-auto">
-                neutral
-              </div>
-              <div className="badge bg-website-purple text-white px-5 py-4 mx-1 w-auto">
-                primary
-              </div>
-              <div className="badge bg-website-purple text-white px-5 py-4 mx-1 w-auto">
-                secondary
-              </div>
-              <div className="badge bg-website-purple text-white px-5 py-4 mx-1 w-auto">
-                accent
-              </div>
-              <div className="badge bg-website-purple text-white px-5 py-4 mx-1 w-auto">
-                ghost
-              </div>
+              {techStack.map((tech, index) => {
+                return <ProjectTechStack key={index} tech={tech} />;
+              })}
             </div>
-            <button className="btn bg-website-purple hover:bg-create text-white rounded-full">
-              Request to join
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="card bg-base-100 w-full shadow-xl border-solid border-2 border-website-purple/25 text-white my-8">
-        <div className="card-body h-96">
-          <div className="flex justify-between items-center text-white">
-            <div className="flex">
-              <i className="fa-regular fa-square text-5xl mr-2"></i>
-              <h2 className="card-title font-bold text-4xl">Project Name</h2>
-            </div>
-            <div className="avatar-group -space-x-6 rtl:space-x-reverse">
-              <div className="avatar">
-                <div className="w-12">
-                  <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                </div>
-              </div>
-              <div className="avatar">
-                <div className="w-12">
-                  <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                </div>
-              </div>
-              <div className="avatar">
-                <div className="w-12">
-                  <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                </div>
-              </div>
-              <div className="avatar">
-                <div className="w-12">
-                  <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <p className="font-light text-xl pt-5">{props.description}</p>
-          <div className="card-actions w-full justify-between items-center ">
-            <div className="w-[60%]">
-              <div className="badge bg-website-purple text-white px-5 py-4 mx-1 w-auto">
-                neutral
-              </div>
-              <div className="badge bg-website-purple text-white px-5 py-4 mx-1 w-auto">
-                primary
-              </div>
-              <div className="badge bg-website-purple text-white px-5 py-4 mx-1 w-auto">
-                secondary
-              </div>
-              <div className="badge bg-website-purple text-white px-5 py-4 mx-1 w-auto">
-                accent
-              </div>
-              <div className="badge bg-website-purple text-white px-5 py-4 mx-1 w-auto">
-                ghost
-              </div>
-            </div>
-            <button className="btn bg-website-purple hover:bg-create text-white rounder-2xl rounded-full">
-              Request to join
-            </button>
+            {acceptingUsers ? (
+              <button className="btn bg-website-purple hover:bg-create text-white rounded-full">
+                Request to join
+              </button>
+            ) : (
+              <button className="btn bg-website-purple hover:bg-create text-white rounded-full">
+                View Project
+              </button>
+            )}
           </div>
         </div>
       </div>
