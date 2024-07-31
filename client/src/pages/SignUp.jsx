3cab466bdecba5ff9cf1db3a6a6cd07afddb7c63
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-31 02:39:43 -0400

    refactor: Update SignIn page layout and styles

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -35,95 +35,110 @@ export const SignUp = () => {
   };
 
   return (
-    <div>
-      <div className="hero bg-base-200 h-screen w-full">
-        <div className="flex-row-reverse flex w-screen items-center justify-center gap-x-72">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Create a CoLab Account</h1>
-            <p className="py-6">
-              Join our community and start exploring exciting projects. Create
-              an account to connect, collaborate, and contribute!
-            </p>
+    <div className="hero bg-base-200 h-screen w-full flex items-center">
+      <div className="container mx-auto flex justify-between items-center h-full px-4">
+
+        <div className="flex flex-col items-center w-1/2 max-w-md bg-base-100 p-8 rounded-lg shadow-lg">
+          <h2 className="text-4xl font-bold mb-4">Sign Up</h2>
+          <form className="w-full" onSubmit={handleAccountCreation}>
+            <div className="form-control mb-4">
+              <label className="label">
+                <span className="label-text">First Name</span>
+              </label>
+              <input
+                type="text"
+                name="first_name"
+                value={formData.first_name}
+                onChange={handleChange}
+                placeholder="Type your first name here"
+                className="input input-bordered w-full"
+                required
+              />
+            </div>
+            <div className="form-control mb-4">
+              <label className="label">
+                <span className="label-text">Last Name</span>
+              </label>
+              <input
+                type="text"
+                name="last_name"
+                value={formData.last_name}
+                onChange={handleChange}
+                placeholder="Type your last name here"
+                className="input input-bordered w-full"
+                required
+              />
+            </div>
+            <div className="form-control mb-4">
+              <label className="label">
+                <span className="label-text">Username</span>
+              </label>
+              <input
+                type="text"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                placeholder="username"
+                className="input input-bordered w-full"
+                required
+              />
+            </div>
+            <div className="form-control mb-4">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="email"
+                className="input input-bordered w-full"
+                required
+              />
+            </div>
+            <div className="form-control mb-4">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                placeholder="password"
+                className="input input-bordered w-full"
+                required
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button type="submit" className="btn btn-primary w-full">
+                Create an account
+              </button>
+            </div>
+          </form>
+          <div className="mt-4 flex flex-col justify-center">
+            <button
+              onClick={() => navigate("/signin")}
+              className="btn btn-link"
+            >
+              Already have an account?
+            </button>
+            <button
+                onClick={() => navigate("/")}
+                className="btn btn-link"
+              >
+                Home Page
+              </button>
           </div>
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <form className="card-body" onSubmit={handleAccountCreation}>
-              <div className="form-control first-name">
-                <label className="label">
-                  <span className="label-text">First Name</span>
-                </label>
-                <input
-                  type="text"
-                  name="first_name"
-                  value={formData.first_name}
-                  onChange={handleChange}
-                  placeholder="Type your first name here"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control last-name">
-                <label className="label">
-                  <span className="label-text">Last Name</span>
-                </label>
-                <input
-                  type="text"
-                  name="last_name"
-                  value={formData.last_name}
-                  onChange={handleChange}
-                  placeholder="Type your last name here"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control username">
-                <label className="label">
-                  <span className="label-text">Username</span>
-                </label>
-                <input
-                  type="text"
-                  name="username"
-                  value={formData.username}
-                  onChange={handleChange}
-                  placeholder="username"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control email">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  placeholder="email"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control password">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  placeholder="password"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control mt-6">
-                <button type="submit" className="btn btn-primary">
-                  Create an account
-                </button>
-              </div>
-            </form>
-          </div>
+        </div>
+
+        <div className="w-1/2 flex flex-col items-center text-center p-8">
+          <h1 className="text-5xl font-bold">Create a CoLab Account</h1>
+          <p className="py-6 text-2xl font-light ">
+            Join our community and start exploring exciting projects. Create an
+            account to connect, collaborate, and contribute!
+          </p>
         </div>
       </div>
     </div>
