6fcf975fa1e9acda99c72f69b62d795b430b01c4
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-30 22:39:00 -0400

    refactor: Improve OwnerProjectAvatar component and add tooltip for owner username

--- client/src/components/Projects/OwnerProjectAvatar.jsx
+++ client/src/components/Projects/OwnerProjectAvatar.jsx
@@ -1,35 +1,24 @@
 import React, { useState } from "react";
 
-export const OwnerProjectAvatar = (props) => {
-  const { owner, owner_username, owner_pic } = props;
-
+export const OwnerProjectAvatar = ({ owner, owner_username, owner_pic }) => {
   const [isHovered, setIsHovered] = useState(false);
 
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
-
-
   return (
-    <div className="flex flex-col w-[19%] items-center justify-center">
-      <div className="tooltip block justify-center items-center rounded-md mt-5">
-        <p className="px-2 w-[55px] text-royal-yellow flex justify-center items-center">{isHovered && `${owner_username}`}</p>
-      </div>
-      <div className="avatar" key={owner}>
-        <div
-          className="w-14 border-royal-yellow border-[3px] rounded-full drop-shadow-yellow-glow"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
-          <img
-            src={`/profile_pics/${owner_pic}`}
-            alt="Owner Avatar"
-          />
-        </div>
+    <div className="flex flex-col items-center justify-center">
+      {isHovered && (
+        <p className="username-item-hover bg-slate-600 rounded-md p-2 text-royal-yellow">
+          {owner_username}
+        </p>
+      )}
+      <div
+        className="avatar w-20 border-royal-yellow border-[3px] rounded-full"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <img
+          src={`/profile_pics/${owner_pic}`}
+          alt={`${owner_username}'s Avatar`}
+        />
       </div>
     </div>
   );
