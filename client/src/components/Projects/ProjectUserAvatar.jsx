d10b37b5d34cf17c05b8c13ef9f082f7fd8aaff6
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-30 22:39:10 -0400

    refactor: Update ProjectUserAvatar component to display participant username on hover

--- client/src/components/Projects/ProjectUserAvatar.jsx
+++ client/src/components/Projects/ProjectUserAvatar.jsx
@@ -1,34 +1,23 @@
 import React, { useState } from "react";
 
-export const ProjectUserAvatar = (props) => {
-  const { participant } = props;
-
+export const ProjectUserAvatar = ({ participant }) => {
   const [isHovered, setIsHovered] = useState(false);
 
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
-
   return (
-    <div className="flex flex-col w-[19%] items-center justify-center">
-      <div className="tooltip flex justify-center items-center rounded-sm">
-        <p className="px-2 flex flex-col">{isHovered && participant.participant_username}</p>
-      </div>
-      <div className="avatar mt-2" key={participant.participant_id}>
-        <div
-          className="w-11"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
-          <img
-            src={`/profile_pics/${participant.participant_pic}`}
-            alt="Participant Avatar"
-          />
-        </div>
+    <div className="flex flex-col items-center justify-center">
+      {isHovered && (
+        <p>{participant.participant_username}</p>
+      )}
+      <div 
+        className="avatar w-20" 
+        key={participant.participant_id}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <img
+          src={`/profile_pics/${participant.participant_pic}`}
+          alt={`${participant.participant_username}'s Avatar`}
+        />
       </div>
     </div>
   );
