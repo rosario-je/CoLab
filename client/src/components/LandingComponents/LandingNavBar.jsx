91ee5cdc74ac076387d1bf17d3a4a9df5ba463ef
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-31 02:39:34 -0400

    refactor: Update LandingNavBar component to handle user authentication and logout

--- client/src/components/LandingComponents/LandingNavBar.jsx
+++ client/src/components/LandingComponents/LandingNavBar.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import vial from "../../images/vial.png";
 import { useNavigate } from "react-router-dom";
 
-export const LandingNavBar = ({ handleCoLabHome }) => {
+export const LandingNavBar = ({
+  currentUser,
+  handleLogout,
+  handleCoLabHome,
+}) => {
   const navigate = useNavigate();
   return (
     <div className="fixed top-0 left-0 right-0 z-50">
@@ -18,14 +22,20 @@ export const LandingNavBar = ({ handleCoLabHome }) => {
         </div>
         <div className="flex-none">
           <div className="signin-button pr-5">
-            <div
-              onClick={() => {
-                navigate("/signin");
-              }}
-              className="btn bg-landing-signin-button rounded-full text-white"
-            >
-              <h2>Sign In</h2>
-            </div>
+            {!currentUser ? (
+              <div
+                onClick={() => {
+                  navigate("/signin");
+                }}
+                className="btn bg-landing-signin-button rounded-full text-white"
+              >
+                <h2>Sign In</h2>
+              </div>
+            ) : (
+                <div className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">
+                  <button onClick={handleLogout}>Logout</button>
+                </div>
+            )}
           </div>
         </div>
       </div>
