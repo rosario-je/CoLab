107387a21403fdc501a79a6c949e635e2e157b43
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-07-31 03:03:13 -0400

    refactor: Update SearchBar component to include search functionality and handle search query

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const SearchBar = () => {
+export const SearchBar = ({ handleSearch }) => {
+  const [query, setQuery] = useState("");
+
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSearch(query);
+    }
+  };
+
   return (
     <div className="search-bar flex justify-center">
-      <label className="input flex  mx-auto fixed items-center gap-2 w-full bg-menu-colors rounded-none h-[65px]">
+      <label className="input flex mx-auto fixed items-center gap-2 w-full bg-menu-colors rounded-none h-[65px]">
         <i className="fa-solid fa-magnifying-glass mx-3 text-[#000000]"></i>
-        <input type="text" className="grow rounded-none" placeholder="Search" />
+        <input
+          type="text"
+          className="grow rounded-none"
+          placeholder="Search"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyPress={onKeyPress}
+        />
         <i className="fa-solid fa-bars mr-10"></i>
       </label>
     </div>
