2d3bccede302fda49f153a6257a77f503a73b10d
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-08-01 02:29:49 -0400

    feat: Fetch project data in ProjectPage component

--- client/src/pages/ProjectPage.jsx
+++ client/src/pages/ProjectPage.jsx
@@ -1,6 +1,7 @@
 // Dashboard.jsx
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 import { Navbar } from "../components/Navbar";
 import { UserLeftMenu } from "../components/UserLeftMenu";
@@ -8,17 +9,46 @@ import { ProjectPageDetails } from "../components/GroupProjectPage/ProjectPageDe
 import { ProjectRightMenu } from "../components/GroupProjectPage/ProjectRightMenu";
 
 export const ProjectPage = ({ currentUser, handleLogout, handleCoLabHome }) => {
-  const navigate = useNavigate();
 
+  const [project, setProject] = useState({
+    name: "",
+    cover_photo_path: "",
+    description: "",
+    figma_link: "",
+    github_repo: "",
+    owner_email: "",
+    owner_pic: "",
+    owner_username: "",
+    participants: [],
+    tech_requirements: [],
+  });
+
+  const { projectId } = useParams();
+
+  useEffect(() => {
+    const fetchProject = async () => {
+      try {
+        const response = await axios.get(`/api/projects/${projectId}`);
+        setProject(response.data);
+      } catch (error) {
+        console.error("Error getting the project", error.message);
+      }
+    };
+    if (projectId) {
+      fetchProject();
+    }
+  }, [projectId]);
+
+console.log(project);
   return (
     <div className="flex flex-col h-screen">
-      <Navbar handleCoLabHome={handleCoLabHome} currentUser={currentUser} />
+      <Navbar handleCoLabHome={handleCoLabHome} currentUser={currentUser} handleLogout={handleLogout}/>
       <div className="flex flex-1 mt-16">
         <UserLeftMenu currentUser={currentUser} />
         <div className="flex grow  justify-center overflow-y-auto mx-72 w-screen">
-          <ProjectPageDetails currentUser={currentUser} />
+          <ProjectPageDetails currentUser={currentUser} project={project}/>
         </div>
-        <ProjectRightMenu currentUser={currentUser} />
+        <ProjectRightMenu currentUser={currentUser} project={project} />
       </div>
     </div>
   );
