536af3d037ab8794af71e59863df82c03a16ea58
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-08-01 19:34:12 -0400

    refactor: Update NotificationsList component styling for responsiveness

--- client/src/components/Notifications/NotificationsList.jsx
+++ client/src/components/Notifications/NotificationsList.jsx
@@ -27,11 +27,11 @@ export const NotificationsList = () => {
   };
 
   return (
-    <div className="my-notifications h-full w-auto flex flex-col items-center mx-72">
+    <div className="my-notifications h-full w-auto flex flex-col items-center">
       <div className="bg-menu-colors p-5 fixed w-full z-10 mx-auto">
         <h1 className="text-white text-2xl mx-72">Notifications</h1>
       </div>
-      <div className="flex flex-col w-full items-center mt-16">
+      <div className="flex flex-col w-full mt-16 items-center">
         {notifications.map((notification) => {
           return (
             <NotificationListItems
