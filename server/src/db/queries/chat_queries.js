c4c6d681a13ed0523f64504561318b13251f69bf
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-08-05 16:08:43 -0400

    feat: Add function to retrieve information for a new chat message

--- server/src/db/queries/chat_queries.js
+++ server/src/db/queries/chat_queries.js
@@ -135,6 +135,31 @@ const dismissNotification = async (notification_id) => {
   }
 };
 
+const getNewChatMessageInfo = async (chat_message_id) => {
+  try {
+    const data = await db.query(
+      `SELECT
+        msg.id AS message_id,
+        msg.sender_id AS sender_id,
+        u.username,
+        u.profile_pic,
+        u.email,
+        msg.message,
+        msg.created_at
+      FROM
+        group_chat_messages msg
+        LEFT JOIN users u ON msg.sender_id = u.id
+      WHERE msg.id = $1
+      ORDER BY msg.created_at ASC;
+      `,
+      [chat_message_id]
+    );
+    return data.rows[0];
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export { 
 createGroupChat, 
 getChatHistory, 
@@ -143,5 +168,6 @@ getProjectChatId,
 sendProjectNotification,
 dismissNotification,
 getNotifications,
-sendJoinNotification
+sendJoinNotification,
+getNewChatMessageInfo
 };
