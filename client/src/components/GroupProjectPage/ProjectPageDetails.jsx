1e2fbbbe6208d49cc03a924973e41d66fd16fd95
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-08-05 16:08:23 -0400

    feat: Add real-time chat functionality with socket.io

--- client/src/components/GroupProjectPage/ProjectPageDetails.jsx
+++ client/src/components/GroupProjectPage/ProjectPageDetails.jsx
@@ -1,24 +1,69 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import io from "socket.io-client";
 import { ProjectUserAvatar } from "../Projects/ProjectUserAvatar";
-
 import { OwnerProjectAvatar } from "../Projects/OwnerProjectAvatar";
 import { ProjectGroupChat } from "./ProjectGroupChat";
 
-export const ProjectPageDetails = ({ project }) => {
+export const ProjectPageDetails = ({ project, fetchProject }) => {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState(project.chat);
+  const socket = useRef(null);
+
+  console.log(chat);
+
   const {
     name,
-    cover_photo_path,
-    description,
-    figma_link,
-    github_repo,
     owner_id,
     owner_email,
     owner_pic,
     owner_username,
     participants,
     tech_requirements,
-    chat,
+    project_id,
   } = project;
+
+  useEffect(() => {
+    socket.current = io("http://localhost:8080");
+
+    socket.current.on("connect", () => {
+      console.log("Connected to server");
+      socket.current.emit("joinProject", project_id);
+    });
+
+    socket.current.on("receiveMessage", (newMessageData) => {
+      console.log("Received message:", newMessageData);
+      setChat((prevChat) => [newMessageData, ...prevChat]);
+    });
+
+    if (project.chat) {
+      setChat(project.chat);
+    }
+
+    // return () => {
+    //   socket.current.disconnect();
+    // };
+  }, [project_id]);
+
+  const handleMessage = async (event) => {
+    if (event.key === "Enter" && message.trim() !== "") {
+      try {
+        const response = await axios.post(`/api/projects/${project_id}/chat`, {
+          message,
+        });
+        const { newMessage } = response.data.data;
+
+        socket.current.emit("sendMessage", {
+          projectId: project_id,
+          message: newMessage,
+        });
+
+        setMessage("");
+      } catch (error) {
+        console.error("Error sending the message", error.message);
+      }
+    }
+  };
 
   return (
     <div className="project-chat-details-container flex flex-col grow mt-3.5">
@@ -57,12 +102,15 @@ export const ProjectPageDetails = ({ project }) => {
         </div>
       </div>
       <div className="chat-main-container relative flex-grow mb-16">
-        <ProjectGroupChat chat={chat}/>
+        <ProjectGroupChat chat={chat} />
       </div>
       <div className="fixed bottom-0 left-[300px] right-[300px] z-20">
         <div className="message-input w-full py-4 px-11 bg-project-background">
           <input
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyDown={handleMessage}
             type="text"
+            value={message}
             placeholder={`Send a message to ${name || "the project"}`}
             className="input input-bordered w-full"
           />
