f48081eb3317e9d151acccd91780c52f72e8a37d
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-08-06 15:40:16 -0400

    refactor: Update ProjectPageDetails component layout and styling

--- client/src/components/GroupProjectPage/ProjectPageDetails.jsx
+++ client/src/components/GroupProjectPage/ProjectPageDetails.jsx
@@ -66,47 +66,48 @@ export const ProjectPageDetails = ({ project, fetchProject }) => {
   };
 
   return (
-    <div className="project-chat-details-container flex flex-col grow mt-3.5">
-      <div className="flex flex-row w-full px-9 justify-between border-b-2 border-project-border/25 h-auto items-center fixed pr-[650px] z-10 backdrop-blur-xl bg-project-left-menu/30">
-        {name && (
-          <div className="project-title">
-            <h1 className="text-text-color font-semibold text-3xl">{name}</h1>
+    <div className="project-container flex flex-col grow mt-3.5">
+      <div className="project-details-container flex flex-row px-6 justify-between border-b-2 border-project-border/25 h-auto items-center  backdrop-blur-xl bg-project-left-menu/30 fixed  top-20 z-10 left-[300px] right-[300px]">
+        <div className="flex flex-col gap-y-6">
+          {name && (
+            <div className="project-title">
+              <h1 className="text-text-color font-semibold text-4xl">{name}</h1>
+            </div>
+          )}
+          <div className="tech-stack flex flex-row justify-start gap-x-6">
+            {tech_requirements &&
+              tech_requirements.map((tech, index) => (
+                <p
+                  key={index}
+                  className="rounded-full bg-website-purple text-white text-sm px-3.5 py-1"
+                >
+                  {tech}
+                </p>
+              ))}
           </div>
-        )}
-        <div className="tech-stack flex flex-row justify-around gap-x-8">
-          {tech_requirements &&
-            tech_requirements.map((tech, index) => (
-              <p
-                key={index}
-                className="rounded-full bg-website-purple text-white text-base px-3.5 py-1"
-              >
-                {tech}
-              </p>
-            ))}
         </div>
         <div className="h-[150px] project-participants-avatars avatar-group flex flex-row flex-start gap-x-3">
           <div className="h-full flex justify-center items-end pb-7">
-          <OwnerProjectAvatar
-            key={owner_id}
-            owner={owner_email}
-            owner_username={owner_username}
-            owner_pic={owner_pic}
-          />
-          {participants &&
-            participants.length > 0 &&
-            participants.map((participant) => (
-              <ProjectUserAvatar
-                key={participant.id}
-                participant={participant}
-                borderColorClass={"border-alt-grey/75"}
-              />
-            ))}
-
+            <OwnerProjectAvatar
+              key={owner_id}
+              owner={owner_email}
+              owner_username={owner_username}
+              owner_pic={owner_pic}
+            />
+            {participants &&
+              participants.length > 0 &&
+              participants.map((participant) => (
+                <ProjectUserAvatar
+                  key={participant.id}
+                  participant={participant}
+                  borderColorClass={"border-alt-grey/75"}
+                />
+              ))}
           </div>
         </div>
       </div>
-      <div className="chat-main-container relative flex-grow mt-32 mb-16">
-        <ProjectGroupChat chat={chat}/>
+      <div className="chat-main-container relative flex-grow mt-[155px] mb-20 bg-alt-grey/75">
+        <ProjectGroupChat chat={chat} />
       </div>
       <div className="fixed bottom-0 left-[300px] right-[300px] z-20">
         <div className="message-input w-full py-4 px-11 bg-project-background">
