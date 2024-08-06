dcbc6dd2486787481c07fb1398e0950cd013a270
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-08-06 15:07:58 -0400

    refactor: Update CreateProject page layout and add project creation header

--- client/src/pages/CreateProject.jsx
+++ client/src/pages/CreateProject.jsx
@@ -15,8 +15,11 @@ export const CreateProject = ({
       <Navbar currentUser={currentUser} />
       <div className="flex flex-1 mt-16">
         <UserLeftMenu currentUser={currentUser} />
-        <div className="flex flex-col w-full h-full bg-project-background overflow-hidden">
+        <div className="flex flex-col w-full h-full bg-project-background overflow-hidden mt-10">
           <div className="flex-grow flex justify-center h-full mx-10 my-5">
+            <div className="flex bg-menu-colors fixed left-[300px] right-[300px] z-10 top-20 h-[65px] justify-start items-center">
+              <h1 className="text-white text-2xl">Create a Project!</h1>
+            </div>
             <ProjectCreateField
               handleTechStacksModal={handleTechStacksModal}
               techModal={techModal}
