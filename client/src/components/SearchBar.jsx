47ab4643932d281bbff7e2f42eab431b35b70521
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-08-06 15:07:22 -0400

    refactor: Update SearchBar component to remove unnecessary icon and improve styling

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -3,23 +3,22 @@ import React, { useState } from "react";
 export const SearchBar = ({ handleSearch }) => {
   const [query, setQuery] = useState("");
 
-  const onKeyPress = (e) => {
+  const handleKeyPress = (e) => {
     if (e.key === "Enter") {
       handleSearch(query);
     }
   };
-
   return (
-    <div className="search-bar flex justify-center">
-      <label className="input flex mx-auto fixed items-center gap-2 w-full bg-menu-colors rounded-none h-[65px]">
-        <i className="fa-solid fa-magnifying-glass mx-3 text-[#000000]"></i>
+    <div className="search-bar z-10 fixed top-20 left-[300px] right-[300px]">
+      <label className="input flex items-center gap-2 bg-menu-colors rounded-none h-[65px]">
+        <i className="fa-solid fa-magnifying-glass mx-3 text-[#ffffff]"></i>
         <input
           type="text"
           className="grow rounded-none"
           placeholder="Search"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
-          onKeyPress={onKeyPress}
+          onKeyDown={handleKeyPress}
         />
       </label>
     </div>
