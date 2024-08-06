4027d675532dd888c0fbb4e86b8ca11ca5011cfc
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-08-06 15:07:47 -0400

    refactor: Update NotificationsList component to improve layout and styling

--- client/src/components/Notifications/NotificationsList.jsx
+++ client/src/components/Notifications/NotificationsList.jsx
@@ -28,8 +28,8 @@ export const NotificationsList = () => {
 
   return (
     <div className="my-notifications h-full w-auto flex flex-col items-center">
-      <div className="bg-menu-colors p-5 fixed w-full z-10 mx-auto">
-        <h1 className="text-white text-2xl mx-72">Notifications</h1>
+      <div className="flex bg-menu-colors fixed left-[300px] right-[300px] z-10 top-20 h-[65px] justify-start items-center">
+        <h1 className="text-white text-2xl">My Notifications</h1>
       </div>
       <div className="flex flex-col w-full mt-16 items-center">
         {notifications.map((notification) => {
