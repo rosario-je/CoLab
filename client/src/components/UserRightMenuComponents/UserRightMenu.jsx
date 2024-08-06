c210c0b9669aff8e861ac8297c535063b8a41f5a
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-08-05 20:28:49 -0400

    refactor: Update UserRightMenu component to fetch and display user's projects in the right menu

--- client/src/components/UserRightMenuComponents/UserRightMenu.jsx
+++ client/src/components/UserRightMenuComponents/UserRightMenu.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { UserMenuProject } from "./UserMenuProject";
+import axios from "axios";
 
-export const UserRightMenu = () => {
+export const UserRightMenu = ({ currentUser }) => {
+  const [rightMenuProjects, setRightMenuProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchRightUserMenu = async () => {
+      try {
+        const response = await axios.get(
+          `/api/dashboard/${currentUser.id}/my_projects`
+        );
+        setRightMenuProjects(response.data);
+      } catch (error) {
+        console.error(
+          "Error getting projects for the right user menu: ",
+          error.message
+        );
+      }
+    };
+    fetchRightUserMenu();
+  }, []);
+  
   return (
     <div className="flex flex-col fixed top-0 right-0 w-[300px] h-full bg-menu-colors justify-between mt-20 pt-16">
       <ul className="menu">
         <li className="border-none">
           <h2 className="menu-title text-text-color text-lg pb-5">Projects</h2>
-          <ul className="text-xl font-light pt-5">
-            <UserMenuProject projectName="Project 1" />
-            <UserMenuProject projectName="Project 2" />
-            <UserMenuProject projectName="Project 3" />
-            <UserMenuProject projectName="Project 4" />
+          <ul className="side-projects text-xl font-light pt-5 space-y-5">
+            {rightMenuProjects.map((project) => {
+              return <UserMenuProject project={project} currentUser={currentUser.id}/>;
+            })}
           </ul>
         </li>
       </ul>
