b549826155016f9c6ce96e8b94d220e08875eee0
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-08-06 15:07:42 -0400

    refactor: Update JoinRequestList component to improve layout and styling

--- client/src/components/MyProjectRequests/JoinRequestList.jsx
+++ client/src/components/MyProjectRequests/JoinRequestList.jsx
@@ -26,8 +26,8 @@ export const JoinRequestList = () => {
 
   return (
     <div className="my-join-requests h-full w-auto flex flex-col items-center mx-72">
-      <div className="bg-menu-colors p-5 fixed w-full z-10 mx-auto">
-        <h1 className="text-white text-2xl mx-72">My Project Requests</h1>
+      <div className="flex bg-menu-colors fixed left-[300px] right-[300px] z-10 top-20 h-[65px] justify-start items-center">
+        <h1 className="text-white text-2xl">My Project Requests</h1>
       </div>
       <div className="flex flex-col w-full items-center mt-16">
         {requests.map((request) => {
