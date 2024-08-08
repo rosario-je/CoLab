4d331f208032275001d40f8d8b1c58dd0cecc1bf
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-08-08 16:55:33 -0400

    refactor: Update ProjectIcon component to handle missing cover photo path

--- client/src/components/GroupProjectPage/ProjectIcon.jsx
+++ client/src/components/GroupProjectPage/ProjectIcon.jsx
@@ -12,7 +12,7 @@ export const ProjectIcon = ({ project, currentUser }) => {
     >
       <img
         className="rounded-2xl w-24 h-24 object-cover border-4 border-website-purple hover:border-website-purple-hover"
-        src={project.cover_photo_path}
+        src={project.cover_photo_path || "https://staticg.sportskeeda.com/editor/2023/05/90701-16836967841966-1920.jpg"}
         alt={project.name}
       />
     </div>
