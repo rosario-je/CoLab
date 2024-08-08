26c380511df25954ec054f7b490be4879b82ebdb
Rebecca <rebecca90469@gmail.com>
2024-08-08 17:16:50 -0400

    Edit to borders and remove unused files

--- client/src/components/GroupProjectPage/ProjectIcon.jsx
+++ client/src/components/GroupProjectPage/ProjectIcon.jsx
@@ -11,7 +11,7 @@ export const ProjectIcon = ({ project, currentUser }) => {
       data-tip={project.name}
     >
       <img
-        className="rounded-2xl w-24 h-24 object-cover border-4 border-text-color/90"
+        className="rounded-2xl w-24 h-24 object-cover border-2 border-text-color/90"
         src={project.cover_photo_path || "https://staticg.sportskeeda.com/editor/2023/05/90701-16836967841966-1920.jpg"}
         alt={project.name}
       />
