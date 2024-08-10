fa7d54b41c793893564673a4a37f496409b868d9
Rebecca <rebecca90469@gmail.com>
2024-08-10 18:38:10 -0400

    Change all pws to 123

--- server/src/db/seeds/01_users_seeds.sql
+++ server/src/db/seeds/01_users_seeds.sql
@@ -1,29 +1,29 @@
 INSERT INTO users (first_name, last_name, username, password, email, profile_pic) VALUES
 -- user 1
-('Naruto', 'Uzumaki', 'naruto', 'password', 'naruto@test.com', 'naruto_pfp.png'),
+('Naruto', 'Uzumaki', 'naruto', '123', 'naruto@test.com', 'naruto_pfp.png'),
 -- user 2
-('Sakura', 'Haruno', 'sakura', 'password', 'sakura@test.com', 'sakura_pfp.png'),
+('Sakura', 'Haruno', 'sakura', '123', 'sakura@test.com', 'sakura_pfp.png'),
 -- user 3
-('Luffy', 'Monkey', 'luffy', 'password', 'luffy@test.com', 'luffy_pfp.png'),
+('Luffy', 'Monkey', 'luffy', '123', 'luffy@test.com', 'luffy_pfp.png'),
 -- user 4
 ('Goku', 'Son', 'goku', '123', 'goku@test.com', 'goku_pfp.png'),
 -- user 5
-('Edward', 'Elric', 'edward', 'password', 'edward@test.com', 'ed_pfp.png'),
+('Edward', 'Elric', 'edward', '123', 'edward@test.com', 'ed_pfp.png'),
 -- user 6
-('Jotaro', 'Kujo', 'jotaro', 'password', 'jotaro@test.com', 'jotaro_pfp.png'),
+('Jotaro', 'Kujo', 'jotaro', '123', 'jotaro@test.com', 'jotaro_pfp.png'),
 -- user 7
-('Light', 'Yagami', 'light', 'password', 'light@test.com', 'light_pfp.png'),
+('Light', 'Yagami', 'light', '123', 'light@test.com', 'light_pfp.png'),
 -- user 8
-('Inu', 'Yasha', 'inuyasha', 'password', 'inuyasha@test.com', 'inuyasha_pfp.png'),
+('Inu', 'Yasha', 'inuyasha', '123', 'inuyasha@test.com', 'inuyasha_pfp.png'),
 -- user 9
-('Mikasa', 'Ackerman', 'mikasa', 'password', 'mikasa@test.com', 'mikasa_pfp.png'),
+('Mikasa', 'Ackerman', 'mikasa', '123', 'mikasa@test.com', 'mikasa_pfp.png'),
 -- user 10
-('Zenitsu', 'Agatsuma', 'zenitsu', 'password', 'zenitsu@test.com', 'zenitsu_pfp.png'),
+('Zenitsu', 'Agatsuma', 'zenitsu', '123', 'zenitsu@test.com', 'zenitsu_pfp.png'),
 -- user 11
 ('Gojo', 'Satoru', 'gojo', '123', 'gojo@test.com', 'gojo_pfp.png'),
 -- user 12
-('Tanjiro', 'Kamado', 'tanjiro', 'password', 'tanjiro@test.com', 'tanjiro_pfp.png'),
+('Tanjiro', 'Kamado', 'tanjiro', '123', 'tanjiro@test.com', 'tanjiro_pfp.png'),
 -- user 13
-('Megumi', 'Fushiguro', 'megumi', 'password', 'megumi@test.com', 'megumi_pfp.png'),
+('Megumi', 'Fushiguro', 'megumi', '123', 'megumi@test.com', 'megumi_pfp.png'),
 -- user 14
-('Reigen', 'Arataka', 'reigen', 'password', 'reigena@test.com', 'reigen_pfp.png');
+('Reigen', 'Arataka', 'reigen', '123', 'reigena@test.com', 'reigen_pfp.png');
