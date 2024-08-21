d651c32dff2109e779776da12cbc543a6ea10daa
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-08-21 19:17:02 -0400

    chore: Update CORS origin to use production URL

--- server/src/index.js
+++ server/src/index.js
@@ -19,7 +19,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://co-lab-livid.vercel.app/",
     methods: ["GET", "POST"]
   }
 });
