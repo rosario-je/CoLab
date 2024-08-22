6503595e89af172fe73fd27c9426f81a548e97a4
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-08-21 22:10:48 -0400

    chore: Update session configuration in index.js

--- server/src/index.js
+++ server/src/index.js
@@ -29,9 +29,11 @@ const sessionOptions = {
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  cookie:{
-      expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-      maxAge: 7 * 24 * 60 * 60 * 1000,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    sameSite: 'None',  
+    secure: true,  
   },
 };
 
