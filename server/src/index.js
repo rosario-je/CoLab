42f9c667d35529a0996f01d4c86d5bb9eb259eca
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-08-21 20:01:21 -0400

    chore: Update CORS origin and methods in index.js

--- server/src/index.js
+++ server/src/index.js
@@ -21,12 +21,14 @@ const io = new Server(server, {
   cors: {
     origin: "https://co-lab-livid.vercel.app",
     methods: ["GET", "POST"]
+    credentials: true
   }
 });
 
 app.use(cors({
   origin: "https://co-lab-livid.vercel.app",
-  methods: ["GET", "POST"]
+  methods: ["GET", "POST"],
+  credentials: true
 }));
 app.options('*', cors());
 app.use(express.json());
