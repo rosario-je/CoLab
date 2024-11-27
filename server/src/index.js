53b7ad0d6c9fd1e20ef7efe79f8fac8efb39ff55
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-11-26 22:08:33 -0500

    chore: Update environment variables and CORS settings for local development

--- server/src/index.js
+++ server/src/index.js
@@ -19,15 +19,15 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://co-lab-mu.vercel.app",
-    methods: ["GET", "POST"],
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT"],
     credentials: true,
   }
 });
 
 app.use(cors({
-  origin: "https://co-lab-mu.vercel.app",
-  methods: ["GET", "POST"],
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT"],
   credentials: true
 }));
 app.use(express.json());
