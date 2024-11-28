0473ee580b9463140eb79ad60c001082b792c4c3
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-11-27 23:37:15 -0500

    fix: Add token validation in MyNotifications component to ensure user authentication

--- client/src/pages/MyNotifications.jsx
+++ client/src/pages/MyNotifications.jsx
@@ -1,9 +1,24 @@
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+//Context
+import { AppContext } from "../context/AppContext";
+
+//Components
 import { UserLeftMenu } from "../components/UserLeftMenu";
 import { UserRightMenu } from "../components/UserRightMenuComponents/UserRightMenu";
 import { Navbar } from "../components/Navbar";
 import { NotificationsList } from "../components/Notifications/NotificationsList";
 
 export const MyNotifications = () => {
+  const { token } = useContext(AppContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      navigate("/");
+    }
+  });
+
   return (
     <div className="flex flex-col h-screen">
       <Navbar />
