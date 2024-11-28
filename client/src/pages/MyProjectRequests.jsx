996101a9741f96b6ce82c3341ed523128cf27da5
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-11-27 23:37:19 -0500

    fix: Add token validation in MyProjectRequests component to ensure user authentication

--- client/src/pages/MyProjectRequests.jsx
+++ client/src/pages/MyProjectRequests.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
+//Context
+import { AppContext } from "../context/AppContext";
+
+//Components
 import { UserLeftMenu } from "../components/UserLeftMenu";
 import { UserRightMenu } from "../components//UserRightMenuComponents/UserRightMenu";
 import { Navbar } from "../components/Navbar";
 import { JoinRequestList } from "../components/MyProjectRequests/JoinRequestList";
 
+
+
 export const MyProjectRequests = () => {
+  const { token } = useContext(AppContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      navigate("/");
+    }
+  })
   return (
     <div className="flex flex-col h-screen">
       <Navbar />
@@ -13,7 +27,7 @@ export const MyProjectRequests = () => {
         <UserLeftMenu />
         <div className="flex flex-col w-full h-full bg-project-background overflow-hidden pt-4">
           <div>
-            <JoinRequestList/>
+            <JoinRequestList />
           </div>
         </div>
         <UserRightMenu />
