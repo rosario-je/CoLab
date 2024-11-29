12b16640bb5419f3149da5bd5fe21195fa1fddcc
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-11-28 19:54:19 -0500

    fix: Simplify ProjectUserAvatar to directly display initials without a separate function

--- client/src/components/Projects/ProjectUserAvatar.jsx
+++ client/src/components/Projects/ProjectUserAvatar.jsx
@@ -3,9 +3,6 @@ import React, { useState } from "react";
 export const ProjectUserAvatar = ({ participant, borderColorClass }) => {
   const [isHovered, setIsHovered] = useState(false);
 
-  // Function to get the first letter of the username
-  const getFirstLetter = (username) => username.charAt(0).toUpperCase();
-
   return (
     <div className="flex flex-col items-center justify-center text-text-color">
       {isHovered && <p>{participant.participant_username}</p>}
@@ -23,7 +20,8 @@ export const ProjectUserAvatar = ({ participant, borderColorClass }) => {
           />
         ) : (
           <span className="text-3xl font-bold text-white bg-gray-500 w-full h-full flex items-center justify-center">
-            {getFirstLetter(participant.participant_username)}
+            {participant.participant_username &&
+              participant.participant_username[0].toUpperCase()}
           </span>
         )}
       </div>
