01f6e0f4f732a4af851d0db739704d4446fc9d20
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-12-04 19:02:29 -0500

    fix: Enhance ProjectCard component for improved responsiveness and layout

--- client/src/components/Projects/ProjectCard.jsx
+++ client/src/components/Projects/ProjectCard.jsx
@@ -13,7 +13,7 @@ export const ProjectCard = ({
   currentUserId,
   project,
   fetchUserProjects,
-  page
+  page,
 }) => {
   const {
     name,
@@ -32,7 +32,8 @@ export const ProjectCard = ({
 
   const socket = useRef(null);
   const { listen, isConnected } = useSocketManager();
-  const { setNotifications, error, setAppError, clearAppError, config } = useContext(AppContext);
+  const { setNotifications, error, setAppError, clearAppError, config } =
+    useContext(AppContext);
 
   useEffect(() => {
     if (isConnected) {
@@ -53,7 +54,8 @@ export const ProjectCard = ({
     e.preventDefault();
     try {
       const requestToJoin = await axios.post(
-        `/api/projects/${project_id}/join`, config
+        `/api/projects/${project_id}/join`,
+        config
       );
       setNotifications((prevNotifications) => [
         requestToJoin.data.data.message,
@@ -75,9 +77,10 @@ export const ProjectCard = ({
         headers: {
           Authorization: `Bearer ${token}`,
         },
-      }
+      };
       const response = await axios.patch(
-        `/api/dashboard/projects/${project_id}/complete`, config
+        `/api/dashboard/projects/${project_id}/complete`,
+        config
       );
       //console.log("Project marked as complete: ", response.data);
       fetchUserProjects();
@@ -87,21 +90,22 @@ export const ProjectCard = ({
   };
 
   return (
-    <div className="card bg-navbar-color w-full shadow-xl border-solid border-2 border-project-border/25 text-text-color/90 my-8">
+    <div className="card bg-navbar-color md:w-full shadow-xl border-solid border-2 border-project-border/25 text-text-color/90 my-5 md:my-8">
       <div className="card-body h-auto">
-        <div className="top-project-card-container flex justify-between items-center mb-5">
-          <div className="project-details-1 flex space-x-6">
+        <div className="top-project-card-container flex flex-col md:flex-row justify-between items-center mb-5">
+          <div className="project-details-1 flex justify-start gap-x-2 md:gap-x-0 w-full md:w-[50%] md:space-x-4">
             <img
               src={
                 cover_photo_path ||
                 "https://staticg.sportskeeda.com/editor/2023/05/90701-16836967841966-1920.jpg"
-              } //<---online
-              //src={`/project_pics/${cover_photo_path}`} // <---not online
+              }
               alt="Project Cover"
-              className="project-cover rounded-xl object-cover h-40 w-40 shadow-2xl border-2 border-text-color"
+              className="project-cover rounded-xl object-cover h-24 w-24 md:h-40 md:w-40 shadow-2xl border-2 border-text-color"
             />
             <div className="flex flex-col justify-center">
-              <h2 className="card-title font-bold text-4xl">{name}</h2>
+              <h2 className="card-title font-bold text-base md:text-xl lg:text-4xl">
+                {name}
+              </h2>
               {page === "myprojects" && isOwner ? (
                 <div className="flex flex-col gap-y-5 my-5 w-full">
                   {is_in_progress ? (
@@ -125,7 +129,7 @@ export const ProjectCard = ({
                     </>
                   ) : (
                     <>
-                      <h3 className="font-semibold text-confirm text-lg ml-2 ">
+                      <h3 className="font-semibold text-confirm md:text-lg md:ml-2 ">
                         Completed
                       </h3>
                     </>
@@ -133,14 +137,17 @@ export const ProjectCard = ({
                 </div>
               ) : (
                 <>
-                  <h3 className="font-semibold mt-5">
-                    <span className="text-icon-purple text-xl">Creator: </span>@
-                    <span className="text-base text-text-color/90">
+                  <h3 className="font-semibold md:mt-5">
+                    <span className="text-icon-purple text-sm lg:text-xl">
+                      Creator:{" "}
+                    </span>
+                    @
+                    <span className="text-sm italic text-text-color/90">
                       {owner_username}{" "}
                     </span>
                   </h3>
                   {!is_in_progress && (
-                    <h3 className="font-semibold text-confirm text-lg mt-3">
+                    <h3 className="font-semibold text-confirm lg:text-lg lg:mt-3">
                       Completed
                     </h3>
                   )}
@@ -148,7 +155,7 @@ export const ProjectCard = ({
               )}
             </div>
           </div>
-          <div className="avatar-group -space-x-6 rtl:space-x-reverse w-2/5 flex justify-end items-end self-end pb-10">
+          <div className="avatar-group  md:-space-x-2 rtl:space-x-reverse w-full justify-start md:w-2/5 lg:flex md:justify-end md:items-end self-end md:pb-10 pt-5 md:pt-0">
             <OwnerProjectAvatar
               owner_username={owner_username}
               owner_pic={owner_pic}
@@ -166,9 +173,11 @@ export const ProjectCard = ({
             )}
           </div>
         </div>
-        <p className="font-light text-xl pt-5 mb-5 italic">{description}</p>
+        <p className="font-light md:text-xl md:pt-5 mb-5 italic">
+          {description}
+        </p>
         <div className="flex card-actions w-full justify-between items-center">
-          <div className="w-[60%] space-y-5">
+          <div className="w-full lg:w-[60%] space-y-2 lg:space-y-5 pb-5 lg:pb-0">
             {tech_requirements.map((tech, index) => (
               <ProjectTechStack key={index} tech={tech} />
             ))}
