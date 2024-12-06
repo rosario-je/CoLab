2211dd77c90ff1adaf72667cd008b9f8547c6795
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-12-05 20:31:01 -0500

    fix: Center align title in NotificationsList component for better presentation

--- client/src/components/Notifications/NotificationsList.jsx
+++ client/src/components/Notifications/NotificationsList.jsx
@@ -8,7 +8,7 @@ export const NotificationsList = () => {
   return (
     <div className="my-notifications h-full w-auto flex flex-col items-center">
       <div className="flex bg-menu-colors fixed left-[300px] right-[300px] z-10 top-20 h-[65px] justify-start items-center">
-        <h1 className="text-white text-2xl">My Notifications</h1>
+        <h1 className="text-white text-2xl w-full text-center">My Notifications</h1>
       </div>
       <div className="flex flex-col w-full mt-16 items-center">
         {notifications.length > 0 ? (
