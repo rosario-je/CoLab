a469f0170dc092570bf9a876b1d3a10e9f5d0f3d
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-12-05 21:06:19 -0500

    fix: Adjust layout and styling in CreateProject component for improved responsiveness and presentation

--- client/src/pages/CreateProject.jsx
+++ client/src/pages/CreateProject.jsx
@@ -17,13 +17,13 @@ export const CreateProject = () => {
     }
   }, [token]);
   return (
-    <div className="flex flex-col h-screen mx-72">
+    <div className="flex flex-col h-screen lg:mx-72">
       <Navbar />
       <div className="flex flex-1 mt-16">
         <UserLeftMenu />
         <div className="flex flex-col w-full h-full bg-project-background overflow-hidden mt-10">
-          <div className="flex-grow flex justify-center h-full mx-10 my-5">
-            <div className="flex bg-menu-colors fixed left-[300px] right-[300px] z-10 top-20 h-[65px] justify-start items-center">
+          <div className="flex-grow flex justify-center h-full lg:mx-10 my-5">
+            <div className="flex bg-menu-colors fixed left-0 right-0 lg:left-[300px] lg:right-[300px] z-10 top-16 lg:top-20 h-[65px] justify-start items-center">
               <h1 className="text-white text-2xl w-full text-center">Create a Project!</h1>
             </div>
             <ProjectCreateField />
