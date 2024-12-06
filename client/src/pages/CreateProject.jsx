5ecd8c69c74d8796702a1f001066ac645aa42e02
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-12-05 20:31:09 -0500

    fix: Center align title in CreateProject component for better presentation

--- client/src/pages/CreateProject.jsx
+++ client/src/pages/CreateProject.jsx
@@ -24,7 +24,7 @@ export const CreateProject = () => {
         <div className="flex flex-col w-full h-full bg-project-background overflow-hidden mt-10">
           <div className="flex-grow flex justify-center h-full mx-10 my-5">
             <div className="flex bg-menu-colors fixed left-[300px] right-[300px] z-10 top-20 h-[65px] justify-start items-center">
-              <h1 className="text-white text-2xl">Create a Project!</h1>
+              <h1 className="text-white text-2xl w-full text-center">Create a Project!</h1>
             </div>
             <ProjectCreateField />
           </div>
