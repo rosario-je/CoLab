eb6f3997c0d98d68244f32baa1bb70253f68b97c
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-12-07 21:45:05 -0500

    fix: Refactor layout in ProjectRightMenu for improved responsiveness and cleaner styling

--- client/src/components/GroupProjectPage/ProjectRightMenu.jsx
+++ client/src/components/GroupProjectPage/ProjectRightMenu.jsx
@@ -37,9 +37,9 @@ export const ProjectRightMenu = ({ project, owner, handleCompleteProject }) => {
   const isOwner = owner === currentUser.id;
 
   return (
-    <div className="hidden lg:flex flex-col fixed top-0 right-0 w-[300px] h-full bg-menu-colors mt-0 pt-24 z-10 pb-4">
-      <div className="right-menu-items-top flex flex-row justify-around h-full">
-        <div className="project-links bg-alt-grey w-24 flex flex-col items-center rounded-xl p-4 space-y-10 justify-top h-full">
+    <div className="hidden lg:flex flex-col fixed top-20 right-0 lg:w-[150px] 2xl:w-[300px] h-full bg-menu-colors mt-0 z-10 p-2 xl:px-0 justify-between pb-24">
+      <div className="right-menu-items-top flex flex-row justify-around h-auto">
+        <div className="project-links bg-alt-grey w-auto 2xl:w-24 flex flex-col items-center rounded-xl p-4  space-y-10 justify-top h-auto">
           <div>
             <a href={project.github_repo} target="_blank">
               <i className="fa-brands fa-github text-6xl pt-10"></i>
@@ -56,8 +56,8 @@ export const ProjectRightMenu = ({ project, owner, handleCompleteProject }) => {
             </a>
           </div>
         </div>
-        <div className="project-list bg-project-left-menu bg-alt-grey w-40 h-full flex flex-col items-center rounded-xl p-4 gap-y-7 justify-start">
-          {rightMenuProjects.map((project) => {
+        <div className="hidden project-list bg-project-left-menu bg-alt-grey w-40 h-auto 2xl:flex flex-col items-center rounded-xl p-4 gap-y-7 justify-start">
+          {rightMenuProjects.slice(0, 4).map((project) => {
             return (
               <ProjectIcon
                 key={project.project_id}
@@ -70,9 +70,9 @@ export const ProjectRightMenu = ({ project, owner, handleCompleteProject }) => {
       </div>
 
       {isOwner && project.is_in_progress && (
-        <div className="flex flex-col grow items-center justify-center space-y-10 p-10">
+        <div className="flex flex-col items-center justify-center space-y-10">
           <button
-            className="bg-icon-purple text-white text-base hover:bg-icon-purple-hover rounded-full w-60 p-1 h-10 font-semibold"
+            className="bg-icon-purple text-white text-base hover:bg-icon-purple-hover rounded-full w-28 2xl:w-60 p-1 h-10 font-semibold"
             onClick={() => {
               navigate(`/${currentUser.id}/project/${project.project_id}/edit`);
             }}
@@ -80,7 +80,7 @@ export const ProjectRightMenu = ({ project, owner, handleCompleteProject }) => {
             Edit
           </button>
           <button
-            className="bg-icon-purple text-white text-base hover:bg-icon-purple-hover rounded-full w-60 p-1 h-10 font-semibold"
+            className="bg-icon-purple text-white text-base hover:bg-icon-purple-hover rounded-full w-28 2xl:w-60 p-1 h-10 font-semibold"
             onClick={handleCompleteProject}
           >
             Complete
