56a29faf3828802597bb9fddbd2b56394357c516
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-12-07 21:56:54 -0500

    fix: Refactor daisyui import in Tailwind configuration

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -91,6 +92,6 @@ export default {
     },
   },
   plugins: [
-    require('daisyui'),
+    daisyui,
   ],
 };
