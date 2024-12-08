e45571f6faf89221875078ba30aa6f71fd147099
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-12-07 21:45:13 -0500

    fix: Improve layout and styling in JoinRequestList for better responsiveness and readability

--- client/src/components/MyProjectRequests/JoinRequestList.jsx
+++ client/src/components/MyProjectRequests/JoinRequestList.jsx
@@ -5,9 +5,9 @@ import { JoinRequestListItems } from "./JoinRequestListItems";
 export const JoinRequestList = () => {
   const { requests } = useContext(AppContext);
   return (
-    <div className="my-join-requests h-full w-auto flex flex-col items-center lg:mx-72">
-      <div className="flex bg-menu-colors fixed left-0 right-0 lg:left-[300px] lg:right-[300px] z-10 top-16 lg:top-20 h-[65px] justify-start items-center">
-        <h1 className="text-white text-2xl w-full text-center">My Project Requests</h1>
+    <div className="my-join-requests h-full w-auto flex flex-col items-center lg:mx-48 2xl:mx-72">
+      <div className="flex bg-menu-colors fixed left-0 right-0 lg:left-[200px] lg:right-[200px] 2xl:left-[300px] 2xl:right-[300px] z-10 top-16 lg:top-20 h-[65px] justify-start items-center">
+        <h1 className="text-white text-xl md:text-base  w-full text-center">My Project Requests</h1>
       </div>
       <div className="flex flex-col w-full px-5 mt-16 items-center">
         {requests.length > 0 ? (
@@ -21,7 +21,7 @@ export const JoinRequestList = () => {
             />
           ))
         ) : (
-          <p>No requests available</p>
+          <p className="text-sm">No requests available</p>
         )}
       </div>
     </div>
