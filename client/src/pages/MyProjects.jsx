e74eb5d34246c6fd607e8489ab9a6d4e9b79f079
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-12-07 21:46:08 -0500

    fix: Update layout and styling in MyProjects for improved responsiveness and readability

--- client/src/pages/MyProjects.jsx
+++ client/src/pages/MyProjects.jsx
@@ -49,9 +49,9 @@ export const MyProjects = ({ handleCoLabHome }) => {
       <Navbar handleCoLabHome={handleCoLabHome} />
       <div className="flex flex-1 mt-16">
         <UserLeftMenu />
-        <div className="flex flex-col w-full bg-project-background overflow-hidden lg:mx-72 px-5 lg:px-10 mt-16 lg:mt-20">
-          <div className="flex bg-menu-colors fixed left-0 right-0 lg:left-[300px] lg:right-[300px] z-10 top-16 lg:top-20 h-[65px] justify-start items-center">
-            <h1 className="text-white text-2xl w-full text-center">My Projects</h1>
+        <div className="flex flex-col grow justify-center overflow-y-auto lg:mx-48 2xl:mx-72 mt-16 px-5 lg:px-10 h-max bg-project-background">
+          <div className="flex bg-menu-colors fixed left-0 right-0 lg:left-[200px] lg:right-[200px] 2xl:left-[300px] 2xl:right-[300px] z-10 top-16 lg:top-20 h-[65px] justify-start items-center">
+            <h1 className="text-white text-xl md:text-base  w-full text-center">My Projects</h1>
           </div>
           {fetchingProjects ? (
             <Skeletons />
