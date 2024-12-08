5ba9b7510f9b30f5a4290fb6de402c1edfb1ce5c
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-12-07 21:45:16 -0500

    fix: Adjust layout and styling in NotificationsList for improved responsiveness and readability

--- client/src/components/Notifications/NotificationsList.jsx
+++ client/src/components/Notifications/NotificationsList.jsx
@@ -6,9 +6,9 @@ export const NotificationsList = () => {
   const { notifications } = useContext(AppContext);
 
   return (
-    <div className="my-notifications h-full w-auto flex flex-col items-center lg:mx-72">
-      <div className="flex bg-menu-colors fixed left-0 right-0 lg:left-[300px] lg:right-[300px] z-10 top-16 lg:top-20 h-[65px] justify-start items-center">
-        <h1 className="text-white text-2xl w-full text-center">My Notifications</h1>
+    <div className="my-notifications h-full w-auto flex flex-col items-center lg:mx-48 2xl:mx-72">
+      <div className="flex bg-menu-colors fixed left-0 right-0 lg:left-[200px] lg:right-[200px] 2xl:left-[300px] 2xl:right-[300px] z-10 top-16 lg:top-20 h-[65px] justify-start items-center">
+        <h1 className="text-white text-xl md:text-base  w-full text-center">My Notifications</h1>
       </div>
       <div className="flex flex-col w-full px-5 mt-16 items-center">
         {notifications.length > 0 ? (
@@ -20,7 +20,7 @@ export const NotificationsList = () => {
             />
           )) // Added closing parenthesis here
         ) : (
-          <p>No notifications available</p>
+          <p className="text-sm">No notifications available</p>
         )}
       </div>
     </div>
