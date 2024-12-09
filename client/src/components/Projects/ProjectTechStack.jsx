cc042ae463c96ae20630182c97dee1fe71d62642
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-12-09 12:11:07 -0500

    fix: Update ProjectTechStack layout for improved text presentation and spacing

--- client/src/components/Projects/ProjectTechStack.jsx
+++ client/src/components/Projects/ProjectTechStack.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export const ProjectTechStack = (props) => {
   const { tech } = props;
   return (
-    <div className="badge bg-website-purple text-white px-5 py-4 mx-1 w-auto text-base">
-      {tech}
+    <div className="badge bg-website-purple text-white px-3 py-4 mx-1 w-auto text-base">
+      <h3 className="font-primary text-[13px] md:text-base">{tech}</h3>
     </div>
   );
 };
