1a5746614719fe122888d156d0e46d21f30bbcbe
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-12-09 12:09:43 -0500

    fix: Adjust height of header and margin for improved layout and spacing

--- client/src/components/MyProjectRequests/JoinRequestList.jsx
+++ client/src/components/MyProjectRequests/JoinRequestList.jsx
@@ -6,10 +6,10 @@ export const JoinRequestList = () => {
   const { requests } = useContext(AppContext);
   return (
     <div className="my-join-requests h-full w-auto flex flex-col items-center lg:mx-48 2xl:mx-72">
-      <div className="flex bg-menu-colors fixed left-0 right-0 lg:left-[200px] lg:right-[200px] 2xl:left-[300px] 2xl:right-[300px] z-10 top-16 lg:top-20 h-[65px] justify-start items-center">
+      <div className="flex bg-menu-colors fixed left-0 right-0 lg:left-[200px] lg:right-[200px] 2xl:left-[300px] 2xl:right-[300px] z-10 top-16 lg:top-20 h-[45px] md:h-[65px] justify-start items-center">
         <h1 className="text-white text-xl md:text-base  w-full text-center">My Project Requests</h1>
       </div>
-      <div className="flex flex-col w-full px-5 mt-16 items-center">
+      <div className="flex flex-col w-full px-5 mt-10 items-center">
         {requests.length > 0 ? (
           requests.map((request) => (
             <JoinRequestListItems
